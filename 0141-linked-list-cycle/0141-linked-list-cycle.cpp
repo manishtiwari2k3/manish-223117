e5198c30ccb633bc030863cf1f970c1b6b02bc0b
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-09-21 20:18:49 +0530

    Time: 20 ms (6.28%), Space: 15.1 MB (7.22%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,6 +9,20 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        // map <ListNode* , int> mpp;
+        // ListNode* temp = head;
+        // while(temp!=NULL){
+        //     if(mpp.find(temp)!=mpp.end()){
+        //         return true;
+        //     }
+        //     mpp[temp] = 1;
+        //     temp = temp->next;
+        // }
+        // return false;
+
+
+
+
         map <ListNode* , int> mpp;
         ListNode* temp = head;
         while(temp!=NULL){
