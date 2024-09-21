6cf6b46a9dd663941b27be7dc8a58e705758c896
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-09-21 20:33:46 +0530

    Time: 8 ms (71.29%), Space: 10.9 MB (67.65%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -23,14 +23,32 @@ public:
 
 
 
-        map <ListNode* , int> mpp;
-        ListNode* temp = head;
-        while(temp!=NULL){
-            if(mpp.find(temp)!=mpp.end()){
+        // map <ListNode* , int> mpp;
+        // ListNode* temp = head;
+        // while(temp!=NULL){
+        //     if(mpp.find(temp)!=mpp.end()){
+        //         return true;
+        //     }
+        //     mpp[temp] = 1;
+        //     temp = temp->next;
+        // }
+        // return false;
+
+
+
+        // optimal
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            // if(fast==slow){
+            //     return true;
+            // }
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast==slow){
                 return true;
             }
-            mpp[temp] = 1;
-            temp = temp->next;
         }
         return false;
     }
