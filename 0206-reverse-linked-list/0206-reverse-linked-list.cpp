cb52543f2d9a350ed423b0df8db7d0d77a1afc05
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-09-21 12:02:33 +0530

    Time: 4 ms (75.92%), Space: 12.9 MB (79.43%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -27,17 +27,30 @@ public:
 
 
 
-        if(head==NULL || head->next==NULL){
-            return head;
-        }
-        ListNode* temp = head;
-        ListNode* prev = NULL;
-        while(temp != NULL){
-            ListNode* front = temp->next;
-            temp->next = prev;
-            prev = temp;
-            temp = front;
-        }
+    //     if(head==NULL || head->next==NULL){
+    //         return head;
+    //     }
+    //     ListNode* temp = head;
+    //     ListNode* prev = NULL;
+    //     while(temp != NULL){
+    //         ListNode* front = temp->next;
+    //         temp->next = prev;
+    //         prev = temp;
+    //         temp = front;
+    //     }
+    // return prev;
+
+    if(head==NULL || head->next==NULL){
+        return head;
+    }
+    ListNode* temp = head;
+    ListNode* prev = NULL;
+    while(temp!=NULL){
+        ListNode* front = temp->next;
+        temp->next = prev;
+        prev = temp;
+        temp = front;
+    }
     return prev;
     }   
 };
