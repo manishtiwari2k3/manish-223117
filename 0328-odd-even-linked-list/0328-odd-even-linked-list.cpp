a3c8d69c2d3ac2534619f33ba680853e12bff9ba
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-09-29 10:56:40 +0530

    Time: 13 ms (18.5%), Space: 15.4 MB (57.98%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,34 +11,54 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(head==NULL||head->next==NULL){
+        // if(head==NULL||head->next==NULL){
+        //     return head;
+        // }
+        // vector <int> arr = {};
+        // ListNode* temp = head;
+        // while(temp!=NULL && temp->next!=NULL){
+        //     arr.emplace_back(temp->val);
+        //     temp=temp->next->next;
+        // }
+        // if(temp){
+        //     arr.emplace_back(temp->val);
+        // }
+        // temp = head->next;
+        // while(temp!=NULL && temp->next!=NULL){
+        //     arr.emplace_back(temp->val);
+        //     temp = temp->next->next;
+        // }
+        // if(temp){
+        //     arr.emplace_back(temp->val);
+        // }
+
+        // int i=0 ;
+        // temp = head;
+        // while(temp){
+        //     temp->val = arr[i];
+        //     temp  = temp->next;
+        //     i++;
+        // }
+        // return head;
+
+
+        // optimal
+
+        if(head==NULL || head->next==NULL){
             return head;
         }
-        vector <int> arr = {};
-        ListNode* temp = head;
-        while(temp!=NULL && temp->next!=NULL){
-            arr.emplace_back(temp->val);
-            temp=temp->next->next;
-        }
-        if(temp){
-            arr.emplace_back(temp->val);
-        }
-        temp = head->next;
-        while(temp!=NULL && temp->next!=NULL){
-            arr.emplace_back(temp->val);
-            temp = temp->next->next;
-        }
-        if(temp){
-            arr.emplace_back(temp->val);
-        }
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenH = head->next;
+        while(even!=NULL && even->next!=NULL)//because even is always ahead of odd so we dont have to check for the odd separately
+        {
+            odd->next = odd->next->next;
+            even->next = even->next->next;
 
-        int i=0 ;
-        temp = head;
-        while(temp){
-            temp->val = arr[i];
-            temp  = temp->next;
-            i++;
+            odd = odd->next;
+            even = even->next;
         }
+        odd->next = evenH;
         return head;
     }
 };
