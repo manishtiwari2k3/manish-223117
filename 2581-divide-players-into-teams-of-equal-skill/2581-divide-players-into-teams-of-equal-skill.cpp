5874fe98ff4246bdcd0bc143bb7dfcf9e8f9cabb
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-10-04 21:43:55 +0530

    Time: 59 ms (80.2%), Space: 55.8 MB (98.4%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
@@ -1,19 +1,54 @@
+// class Solution {
+// public:
+//     long long dividePlayers(vector<int>& skill) {
+//         sort(skill.begin(), skill.end());
+
+//         int n = skill.size();
+//         long long totalChemistry = 0;
+//         int targetTeamSkill = skill[0] + skill[n - 1];
+//         for (int i = 0; i < n / 2; i++) {
+//             int currentTeamSkill = skill[i] + skill[n - i - 1];
+//             if (currentTeamSkill != targetTeamSkill) {
+//                 return -1;
+//             }
+//             totalChemistry += (long long)skill[i] * (long long)skill[n - i - 1];
+//         }
+
+//         return totalChemistry;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
-        sort(skill.begin(), skill.end());
-
         int n = skill.size();
-        long long totalChemistry = 0;
-        int targetTeamSkill = skill[0] + skill[n - 1];
-        for (int i = 0; i < n / 2; i++) {
-            int currentTeamSkill = skill[i] + skill[n - i - 1];
-            if (currentTeamSkill != targetTeamSkill) {
-                return -1;
-            }
-            totalChemistry += (long long)skill[i] * (long long)skill[n - i - 1];
+        int totalSkill = 0;
+        vector<int> skillFrequency(1001, 0);
+
+        for (int playerSkill : skill) {
+            totalSkill += playerSkill;
+            skillFrequency[playerSkill]++;
         }
 
-        return totalChemistry;
+        if (totalSkill % (n / 2) != 0) {
+            return -1;
+        }
+
+        int targetTeamSkill = totalSkill / (n / 2);
+        long long totalChemistry = 0;
+        for (int playerSkill : skill) {
+            int partnerSkill = targetTeamSkill - playerSkill;
+
+            if (skillFrequency[partnerSkill] == 0) {
+                return -1;
+            }
+
+            totalChemistry += (long long)playerSkill * (long long)partnerSkill;
+            skillFrequency[partnerSkill]--;
+        }
+        return totalChemistry / 2;
     }
 };
