1c66a16fd83f8c97625b37266ef14a2c72758432
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-10-06 19:33:56 +0530

    Time: 5 ms (8.76%), Space: 9.1 MB (7.37%) - LeetHub

--- 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    bool areSentencesSimilar(std::string s1, std::string s2) {
-        std::istringstream iss1(s1);
-        std::deque<std::string> deque1(
-            (std::istream_iterator<std::string>(iss1)),
-            std::istream_iterator<std::string>());
-        std::istringstream iss2(s2);
-        std::deque<std::string> deque2(
-            (std::istream_iterator<std::string>(iss2)),
-            std::istream_iterator<std::string>());
-        while (!deque1.empty() && !deque2.empty() &&
-               deque1.front() == deque2.front()) {
-            deque1.pop_front();
-            deque2.pop_front();
+    bool areSentencesSimilar(string s1, string s2) {
+        stringstream ss1(s1), ss2(s2);
+        string word;
+        vector<string> s1Words, s2Words;
+        while (ss1 >> word) s1Words.push_back(word);
+        while (ss2 >> word) s2Words.push_back(word);
+
+        int start = 0, ends1 = s1Words.size() - 1, ends2 = s2Words.size() - 1;
+
+        if (s1Words.size() > s2Words.size()) return areSentencesSimilar(s2, s1);
+
+        while (start < s1Words.size() && s1Words[start] == s2Words[start])
+            ++start;
+
+        while (ends1 >= 0 && s1Words[ends1] == s2Words[ends2]) {
+            --ends1;
+            --ends2;
         }
-        while (!deque1.empty() && !deque2.empty() &&
-               deque1.back() == deque2.back()) {
-            deque1.pop_back();
-            deque2.pop_back();
-        }
-        return deque1.empty() || deque2.empty();
+        return ends1 < start;
     }
 };
