428619e791e827c8a71dd569432077733ee0670d
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-10-12 19:01:23 +0530

    Time: 502 ms (6.08%), Space: 134 MB (30.41%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,18 +1,43 @@
+// class Solution {
+// public:
+//     int minGroups(vector<vector<int>>& intervals) {
+//         vector<pair<int, int>> intervalsWithEnd;
+//         for (vector<int> interval : intervals) {
+//             intervalsWithEnd.push_back({interval[0], 1});
+//             intervalsWithEnd.push_back({interval[1] + 1, -1});
+//         }
+
+//         sort(intervalsWithEnd.begin(), intervalsWithEnd.end());
+
+//         int concurrentIntervals = 0;
+//         int maxConcurrentIntervals = 0;
+//         for (auto i : intervalsWithEnd) {
+//             concurrentIntervals += i.second;
+//             maxConcurrentIntervals =
+//                 max(maxConcurrentIntervals, concurrentIntervals);
+//         }
+
+//         return maxConcurrentIntervals;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
-        vector<pair<int, int>> intervalsWithEnd;
+        map<int, int> pointToCount;
         for (vector<int> interval : intervals) {
-            intervalsWithEnd.push_back({interval[0], 1});
-            intervalsWithEnd.push_back({interval[1] + 1, -1});
+            pointToCount[interval[0]]++;
+            pointToCount[interval[1] + 1]--;
         }
-
-        sort(intervalsWithEnd.begin(), intervalsWithEnd.end());
 
         int concurrentIntervals = 0;
         int maxConcurrentIntervals = 0;
-        for (auto i : intervalsWithEnd) {
-            concurrentIntervals += i.second;
+        for (pair<int, int> p : pointToCount) {
+            concurrentIntervals += p.second;
+
             maxConcurrentIntervals =
                 max(maxConcurrentIntervals, concurrentIntervals);
         }
