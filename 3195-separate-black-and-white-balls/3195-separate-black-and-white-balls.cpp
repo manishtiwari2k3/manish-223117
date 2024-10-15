bae61ae8b435da3dbc4a3c274e14a18bb806a888
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-10-15 22:11:15 +0530

    Time: 39 ms (21.05%), Space: 15.7 MB (45.68%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -1,37 +1,37 @@
-// class Solution {
-// public:
-//     long long minimumSteps(string s) {
-//         int whitePosition = 0;
-//         long long totalSwaps = 0;
-//         for (int currentPos = 0; currentPos < s.length(); currentPos++) {
-//             if (s[currentPos] == '0') {
-//                 totalSwaps += currentPos - whitePosition;
-
-//                 whitePosition++;
-//             }
-//         }
-
-//         return totalSwaps;
-//     }
-// };
-
-
-
-
 class Solution {
 public:
     long long minimumSteps(string s) {
+        int whitePosition = 0;
         long long totalSwaps = 0;
-        int blackBallCount = 0;
+        for (int currentPos = 0; currentPos < s.length(); currentPos++) {
+            if (s[currentPos] == '0') {
+                totalSwaps += currentPos - whitePosition;
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '0') {
-                totalSwaps += (long long)blackBallCount;
-            } else {
-                blackBallCount++;
+                whitePosition++;
             }
         }
 
         return totalSwaps;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     long long minimumSteps(string s) {
+//         long long totalSwaps = 0;
+//         int blackBallCount = 0;
+
+//         for (int i = 0; i < s.length(); i++) {
+//             if (s[i] == '0') {
+//                 totalSwaps += (long long)blackBallCount;
+//             } else {
+//                 blackBallCount++;
+//             }
+//         }
+
+//         return totalSwaps;
+//     }
+// };
