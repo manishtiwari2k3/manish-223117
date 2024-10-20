2e9b543db2166c9c8ea2a18425469e6b0b5ef6c6
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-10-20 18:26:49 +0530

    Time: 4 ms (86.18%), Space: 15.8 MB (8.78%) - LeetHub

--- 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.cpp
+++ 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.cpp
@@ -1,25 +1,76 @@
+// class Solution {
+// public:
+//     bool parseBoolExpr(string expression) {
+//         while (expression.length() > 1) {
+//             int start = expression.find_last_of("!&|");
+//             int end = expression.find(')', start);
+//             string subExpr = expression.substr(start, end - start + 1);
+//             char result = evaluateSubExpr(subExpr);
+//             expression.replace(start, end - start + 1, 1,
+//                                result);  
+//         }
+//         return expression[0] == 't';
+//     }
+
+// private:
+//     char evaluateSubExpr(const string& subExpr) {
+//         char op = subExpr[0];
+//         string values = subExpr.substr(2, subExpr.length() - 3);
+//         if (op == '!') return values[0] == 't' ? 'f' : 't';
+//         if (op == '&') return values.find('f') != string::npos ? 'f' : 't';
+//         if (op == '|') return values.find('t') != string::npos ? 't' : 'f';
+
+//         return 'f';  
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     bool parseBoolExpr(string expression) {
-        while (expression.length() > 1) {
-            int start = expression.find_last_of("!&|");
-            int end = expression.find(')', start);
-            string subExpr = expression.substr(start, end - start + 1);
-            char result = evaluateSubExpr(subExpr);
-            expression.replace(start, end - start + 1, 1,
-                               result);  
-        }
-        return expression[0] == 't';
+        int index = 0;
+        return evaluate(expression, index);
     }
 
-private:
-    char evaluateSubExpr(const string& subExpr) {
-        char op = subExpr[0];
-        string values = subExpr.substr(2, subExpr.length() - 3);
-        if (op == '!') return values[0] == 't' ? 'f' : 't';
-        if (op == '&') return values.find('f') != string::npos ? 'f' : 't';
-        if (op == '|') return values.find('t') != string::npos ? 't' : 'f';
+public:
+    bool evaluate(string& expr, int& index) {
+        char currChar = expr[index++];
 
-        return 'f';  
+        if (currChar == 't') return true;
+        if (currChar == 'f') return false;
+        if (currChar == '!') {
+            index++;  
+            bool result = !evaluate(expr, index);
+            index++;  
+            return result;
+        }
+        vector<bool> values;
+        index++; 
+        while (expr[index] != ')') {
+            if (expr[index] != ',') {
+                values.push_back(evaluate(
+                    expr, index));  
+            } else {
+                index++; 
+            }
+        }
+        index++;  
+        if (currChar == '&') {
+            for (bool v : values) {
+                if (!v) return false;
+            }
+            return true;
+        }
+
+        if (currChar == '|') {
+            for (bool v : values) {
+                if (v) return true;
+            }
+            return false;
+        }
+
+        return false;  
     }
 };
