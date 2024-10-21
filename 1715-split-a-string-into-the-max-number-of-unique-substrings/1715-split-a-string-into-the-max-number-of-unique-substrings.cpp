fc77255af333f3c2bdc8f0549b05e6f1e66241c7
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-10-21 18:05:02 +0530

    Time: 15 ms (99.42%), Space: 12.8 MB (93.86%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,23 +1,54 @@
+// class Solution {
+// public:
+//     int maxUniqueSplit(string s) {
+//         unordered_set<string> seen;
+//         return backtrack(s, 0, seen);
+//     }
+
+// private:
+//     int backtrack(const string& s, int start, unordered_set<string>& seen) {
+//         if (start == s.size()) return 0;
+
+//         int maxCount = 0;
+//         for (int end = start + 1; end <= s.size(); ++end) {
+//             string substring = s.substr(start, end - start);
+//             if (seen.find(substring) == seen.end()) {
+//                 seen.insert(substring);
+//                 maxCount = max(maxCount, 1 + backtrack(s, end, seen));
+//                 seen.erase(substring);
+//             }
+//         }
+//         return maxCount;
+//     }
+// };
+
+
 class Solution {
 public:
     int maxUniqueSplit(string s) {
         unordered_set<string> seen;
-        return backtrack(s, 0, seen);
+        int maxCount = 0;
+        backtrack(s, 0, seen, 0, maxCount);
+        return maxCount;
     }
 
 private:
-    int backtrack(const string& s, int start, unordered_set<string>& seen) {
-        if (start == s.size()) return 0;
+    void backtrack(const string& s, int start, unordered_set<string>& seen,
+                   int count, int& maxCount) {
+        if (count + (s.size() - start) <= maxCount) return;
 
-        int maxCount = 0;
+        if (start == s.size()) {
+            maxCount = max(maxCount, count);
+            return;
+        }
+
         for (int end = start + 1; end <= s.size(); ++end) {
             string substring = s.substr(start, end - start);
             if (seen.find(substring) == seen.end()) {
                 seen.insert(substring);
-                maxCount = max(maxCount, 1 + backtrack(s, end, seen));
+                backtrack(s, end, seen, count + 1, maxCount);
                 seen.erase(substring);
             }
         }
-        return maxCount;
     }
 };
