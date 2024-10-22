105289507b967269803fa9fef68586fbf9217b88
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-10-22 13:38:18 +0530

    Time: 16 ms (98.63%), Space: 156.1 MB (5.08%) - LeetHub

--- 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.cpp
+++ 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.cpp
@@ -9,31 +9,63 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     long kthLargestLevelSum(TreeNode* root, int k) {
+//         priority_queue<long> pq;
+
+//         queue<TreeNode*> bfsQueue;
+//         bfsQueue.push(root);
+//         while (!bfsQueue.empty()) {
+//             long size = bfsQueue.size();
+//             long sum = 0;
+//             for (int i = 0; i < size; i++) {
+//                 TreeNode* poppedNode = bfsQueue.front();
+//                 bfsQueue.pop();
+//                 sum += poppedNode->val;
+//                 if (poppedNode->left != nullptr) {
+//                     bfsQueue.push(poppedNode->left);
+//                 }
+//                 if (poppedNode->right != nullptr) {
+//                     bfsQueue.push(poppedNode->right);
+//                 }
+//             }
+//             pq.push(sum);
+//         }
+//         if (pq.size() < k) return -1;
+//         for (int i = 0; i < k - 1; i++) pq.pop();
+//         return pq.top();
+//     }
+// };
+
+
 class Solution {
 public:
     long kthLargestLevelSum(TreeNode* root, int k) {
-        priority_queue<long> pq;
+        priority_queue<long, vector<long>, greater<long> > pq;
 
         queue<TreeNode*> bfsQueue;
         bfsQueue.push(root);
         while (!bfsQueue.empty()) {
-            long size = bfsQueue.size();
+            int size = bfsQueue.size();
             long sum = 0;
             for (int i = 0; i < size; i++) {
                 TreeNode* poppedNode = bfsQueue.front();
                 bfsQueue.pop();
                 sum += poppedNode->val;
-                if (poppedNode->left != nullptr) {
+                if (poppedNode->left != NULL) {
                     bfsQueue.push(poppedNode->left);
                 }
-                if (poppedNode->right != nullptr) {
+                if (poppedNode->right != NULL) {
                     bfsQueue.push(poppedNode->right);
                 }
             }
             pq.push(sum);
+            if (pq.size() > k) {
+                pq.pop();
+            }
         }
         if (pq.size() < k) return -1;
-        for (int i = 0; i < k - 1; i++) pq.pop();
         return pq.top();
     }
 };
