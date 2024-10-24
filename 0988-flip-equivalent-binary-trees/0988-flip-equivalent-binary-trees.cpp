9ae1ac438b769518e0d4cb7fd6196fe1b3cce637
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-10-24 21:51:09 +0530

    Time: 3 ms (50.16%), Space: 14.7 MB (6.32%) - LeetHub

--- 0988-flip-equivalent-binary-trees/0988-flip-equivalent-binary-trees.cpp
+++ 0988-flip-equivalent-binary-trees/0988-flip-equivalent-binary-trees.cpp
@@ -9,24 +9,62 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+//         if (root1 == nullptr && root2 == nullptr) {
+//             return true;
+//         }
+//         if (root1 == nullptr || root2 == nullptr) {
+//             return false;
+//         }
+//         if (root1->val != root2->val) {
+//             return false;
+//         }
+
+//         bool noSwap = flipEquiv(root1->left, root2->left) &&
+//                       flipEquiv(root1->right, root2->right);
+//         bool swap = flipEquiv(root1->left, root2->right) &&
+//                     flipEquiv(root1->right, root2->left);
+
+//         return swap || noSwap;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        if (root1 == nullptr && root2 == nullptr) {
-            return true;
-        }
-        if (root1 == nullptr || root2 == nullptr) {
-            return false;
-        }
-        if (root1->val != root2->val) {
-            return false;
-        }
-
-        bool noSwap = flipEquiv(root1->left, root2->left) &&
-                      flipEquiv(root1->right, root2->right);
-        bool swap = flipEquiv(root1->left, root2->right) &&
-                    flipEquiv(root1->right, root2->left);
-
-        return swap || noSwap;
+    bool checkNodeValues(TreeNode* node1, TreeNode* node2) {
+        if (node1 == nullptr && node2 == nullptr) return true;
+        if (node1 && node2 && node1->val == node2->val) return true;
+        return false;
     }
+
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        stack<pair<TreeNode*, TreeNode*>> nodePairStack;
+        nodePairStack.push({root1, root2});
+
+        while (!nodePairStack.empty()) {
+            auto [node1, node2] = nodePairStack.top();
+            nodePairStack.pop();
+
+            if (!node1 && !node2) continue;
+            if (!node1 || !node2) return false;
+            if (node1->val != node2->val) return false;
+
+            if (checkNodeValues(node1->left, node2->left) &&
+                checkNodeValues(node1->right, node2->right)) {
+                nodePairStack.push({node1->left, node2->left});
+                nodePairStack.push({node1->right, node2->right});
+            } else if (checkNodeValues(node1->left, node2->right) &&
+                       checkNodeValues(node1->right, node2->left)) {
+                nodePairStack.push({node1->left, node2->right});
+                nodePairStack.push({node1->right, node2->left});
+            } else {
+                return false;
+            }
+        }
+        return true;
+    };
 };
