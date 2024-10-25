6daf44032f5d96a8c02a4066154f77cc736b273d
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-10-25 22:45:22 +0530

    Time: 53 ms (93.68%), Space: 47.8 MB (75.82%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -1,26 +1,48 @@
+// class Solution {
+// public:
+//     vector<string> removeSubfolders(vector<string>& folder) {
+//         unordered_set<string> folderSet(folder.begin(), folder.end());
+//         vector<string> result;
+
+//         for (string& f : folder) {
+//             bool isSubFolder = false;
+//             string prefix = f;
+
+//             while (!prefix.empty()) {
+//                 size_t pos = prefix.find_last_of('/');
+//                 if (pos == string::npos) break;
+//                 prefix = prefix.substr(0, pos);
+
+//                 if (folderSet.count(prefix)) {
+//                     isSubFolder = true;
+//                     break;
+//                 }
+//             }
+
+//             if (!isSubFolder) {
+//                 result.push_back(f);
+//             }
+//         }
+
+//         return result;
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
-        unordered_set<string> folderSet(folder.begin(), folder.end());
+        sort(folder.begin(), folder.end());
         vector<string> result;
-
-        for (string& f : folder) {
-            bool isSubFolder = false;
-            string prefix = f;
-
-            while (!prefix.empty()) {
-                size_t pos = prefix.find_last_of('/');
-                if (pos == string::npos) break;
-                prefix = prefix.substr(0, pos);
-
-                if (folderSet.count(prefix)) {
-                    isSubFolder = true;
-                    break;
-                }
-            }
-
-            if (!isSubFolder) {
-                result.push_back(f);
+        result.push_back(folder[0]);
+        for (int i = 1; i < folder.size(); i++) {
+            string lastFolder = result.back();
+            lastFolder += '/';
+            if (folder[i].compare(0, lastFolder.size(), lastFolder) != 0) {
+                result.push_back(folder[i]);
             }
         }
 
