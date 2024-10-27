9a72326c250cf1f318c0f3bafa5cb1d7a3306cef
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-10-27 19:01:08 +0530

    Time: 2 ms (85.6%), Space: 29.1 MB (27.15%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,25 +1,46 @@
+// class Solution {
+// public:
+//     int solve(int i, int j, vector<vector<int>>& grid,
+//               vector<vector<int>>& dp) {
+//         if (i >= grid.size() || j >= grid[0].size()) return 0;
+//         if (grid[i][j] == 0) return 0;
+
+//         if (dp[i][j] != -1) return dp[i][j];
+
+//         int right = solve(i, j + 1, grid, dp);
+//         int diagonal = solve(i + 1, j + 1, grid, dp);
+//         int below = solve(i + 1, j, grid, dp);
+
+//         return dp[i][j] = 1 + min(right, min(diagonal, below));
+//     }
+
+//     int countSquares(vector<vector<int>>& grid) {
+//         int ans = 0;
+//         vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), -1));
+//         for (int i = 0; i < grid.size(); i++) {
+//             for (int j = 0; j < grid[0].size(); j++) {
+//                 ans += solve(i, j, grid, dp);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& grid,
-              vector<vector<int>>& dp) {
-        if (i >= grid.size() || j >= grid[0].size()) return 0;
-        if (grid[i][j] == 0) return 0;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int right = solve(i, j + 1, grid, dp);
-        int diagonal = solve(i + 1, j + 1, grid, dp);
-        int below = solve(i + 1, j, grid, dp);
-
-        return dp[i][j] = 1 + min(right, min(diagonal, below));
-    }
-
-    int countSquares(vector<vector<int>>& grid) {
+    int countSquares(vector<vector<int>>& matrix) {
+        int row = matrix.size(), col = matrix[0].size();
+        vector<vector<int>> dp(row + 1, vector<int>(col + 1, 0));
         int ans = 0;
-        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), -1));
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                ans += solve(i, j, grid, dp);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 1) {
+                    dp[i + 1][j + 1] =
+                        min({dp[i][j + 1], dp[i + 1][j], dp[i][j]}) + 1;
+                    ans += dp[i + 1][j + 1];
+                }
             }
         }
         return ans;
