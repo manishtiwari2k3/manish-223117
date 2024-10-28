e660dd526d54e8136fdf4218688cc9e50010d2de
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-10-28 22:28:41 +0530

    Time: 4 ms (11.39%), Space: 8.5 MB (32.7%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-//         class Solution {
-// public:
-//     int divide(int dividend, int divisor) {
-//         int cnt  = 0;
-//         int sum = 0;
-//         while((sum+divisor)>=dividend){
-//             cnt++;
-//             sum = sum + divisor;
-//         }
-//         return cnt;
-//     }
-// };
-
-
-
-
         if(dividend == divisor){
             return 1;
         }
@@ -29,15 +13,14 @@ public:
         }
         long n = abs(dividend);
         long d = abs(divisor);
-        // divisor = abs(divisor);
         long quotient = 0;
         while(n>=d){
             int cnt = 0;
-            while(n>=(d<<cnt+1)){
+            while(n>=d<<(cnt+1)){
                 cnt++;
             }
             quotient = quotient+(1<<cnt);
-            n = n - (d<<cnt);
+            n = n-(d<<cnt);
         }
         if(quotient==(1<<31) && sign){
             return INT_MAX;
@@ -48,3 +31,54 @@ public:
         return sign ? quotient : -quotient;
     }
 };
+
+// class Solution {
+// public:
+//     int divide(int dividend, int divisor) {
+// //         class Solution {
+// // public:
+// //     int divide(int dividend, int divisor) {
+// //         int cnt  = 0;
+// //         int sum = 0;
+// //         while((sum+divisor)>=dividend){
+// //             cnt++;
+// //             sum = sum + divisor;
+// //         }
+// //         return cnt;
+// //     }
+// // };
+
+
+
+
+//         if(dividend == divisor){
+//             return 1;
+//         }
+//         bool sign = true;
+//         if(dividend<0 && divisor>0){
+//             sign = false;
+//         }
+//         else if(dividend>=0 && divisor<0){
+//             sign = false;
+//         }
+//         long n = abs(dividend);
+//         long d = abs(divisor);
+//         // divisor = abs(divisor);
+//         long quotient = 0;
+//         while(n>=d){
+//             int cnt = 0;
+//             while(n>=(d<<cnt+1)){
+//                 cnt++;
+//             }
+//             quotient = quotient+(1<<cnt);
+//             n = n - (d<<cnt);
+//         }
+//         if(quotient==(1<<31) && sign){
+//             return INT_MAX;
+//         }
+//         if(quotient==(1<<31) && !sign){
+//             return INT_MIN;
+//         }
+//         return sign ? quotient : -quotient;
+//     }
+// };
