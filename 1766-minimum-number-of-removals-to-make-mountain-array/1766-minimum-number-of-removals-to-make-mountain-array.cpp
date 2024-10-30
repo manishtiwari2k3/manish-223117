4fd1d4db21f7e5cb2238688c4eaac30472523a30
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-10-30 10:27:04 +0530

    Time: 0 ms (100%), Space: 15.2 MB (31.84%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,25 +1,69 @@
+// class Solution {
+// public:
+//     int minimumMountainRemovals(vector<int>& nums) {
+//         int N = nums.size();
+
+//         vector<int> lisLength(N , 1);
+//         vector<int> ldsLength(N, 1);
+//         for (int i = 0; i < N; i++) {
+//             for (int j = i - 1; j >= 0; j--) {
+//                 if (nums[i] > nums[j]) {
+//                     lisLength[i] = max(lisLength[i], lisLength[j] + 1);
+//                 }
+//             }
+//         }
+
+//         for (int i = N - 1; i >= 0; i--) {
+//             for (int j = i + 1; j < N; j++) {
+//                 if (nums[i] > nums[j]) {
+//                     ldsLength[i] = max(ldsLength[i], ldsLength[j] + 1);
+//                 }
+//             }
+//         }
+
+//         int minRemovals = INT_MAX;
+//         for (int i = 0; i < N; i++) {
+//             if (lisLength[i] > 1 && ldsLength[i] > 1) {
+//                 minRemovals =
+//                     min(minRemovals, N - lisLength[i] - ldsLength[i] + 1);
+//             }
+//         }
+
+//         return minRemovals;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
+    vector<int> getLongestIncreasingSubsequenceLength(vector<int>& v) {
+        vector<int> lisLen(v.size(), 1);
+        vector<int> lis = {v[0]};
+
+        for (int i = 1; i < v.size(); i++) {
+            int index = lower_bound(lis.begin(), lis.end(), v[i]) - lis.begin();
+
+            if (index == lis.size()) {
+                lis.push_back(v[i]);
+            } else {
+                lis[index] = v[i];
+            }
+
+            lisLen[i] = lis.size();
+        }
+
+        return lisLen;
+    }
+
     int minimumMountainRemovals(vector<int>& nums) {
         int N = nums.size();
+        vector<int> lisLength = getLongestIncreasingSubsequenceLength(nums);
 
-        vector<int> lisLength(N , 1);
-        vector<int> ldsLength(N, 1);
-        for (int i = 0; i < N; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (nums[i] > nums[j]) {
-                    lisLength[i] = max(lisLength[i], lisLength[j] + 1);
-                }
-            }
-        }
-
-        for (int i = N - 1; i >= 0; i--) {
-            for (int j = i + 1; j < N; j++) {
-                if (nums[i] > nums[j]) {
-                    ldsLength[i] = max(ldsLength[i], ldsLength[j] + 1);
-                }
-            }
-        }
+        reverse(nums.begin(), nums.end());
+        vector<int> ldsLength = getLongestIncreasingSubsequenceLength(nums);
+        reverse(ldsLength.begin(), ldsLength.end());
 
         int minRemovals = INT_MAX;
         for (int i = 0; i < N; i++) {
