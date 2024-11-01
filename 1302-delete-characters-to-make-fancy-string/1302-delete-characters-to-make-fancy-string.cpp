a20e24dd8adf9ae5348b374110785f46388a12a1
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-11-01 09:43:41 +0530

    Time: 15 ms (93.51%), Space: 36.4 MB (96.28%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,22 +1,43 @@
+// class Solution {
+// public:
+//     string makeFancyString(string s) {
+//         char prev = s[0];
+//         int frequency = 1;
+//         string ans = "";
+//         ans.push_back(s[0]);
+
+//         for (int i = 1; i < s.size(); i++) {
+//             if (s[i] == prev) {
+//                 frequency++;
+//             } else {
+//                 prev = s[i];
+//                 frequency = 1;
+//             }
+
+//             if (frequency < 3) ans.push_back(s[i]);
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     string makeFancyString(string s) {
-        char prev = s[0];
-        int frequency = 1;
-        string ans = "";
-        ans.push_back(s[0]);
-
-        for (int i = 1; i < s.size(); i++) {
-            if (s[i] == prev) {
-                frequency++;
-            } else {
-                prev = s[i];
-                frequency = 1;
+        if (s.length() < 3) {
+            return s;
+        }
+        int j = 2;
+        for (int i = 2; i < s.size(); ++i) {
+            if (s[i] != s[j - 1] || s[i] != s[j - 2]) {
+                s[j++] = s[i];
             }
-
-            if (frequency < 3) ans.push_back(s[i]);
         }
 
-        return ans;
+        s.resize(j);
+        return s;
     }
 };
