ef127f1575572c9b7b1689f820d1cce6a074e74e
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-11-01 09:39:19 +0530

    Time: 4 ms (27.09%), Space: 19.5 MB (86.56%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,15 +1,25 @@
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         map<int,int> mpp;
+//         for(int i=0;i<nums.size();i++){
+//             mpp[nums[i]]++;
+//         }
+//         for(auto it:mpp){
+//             if(it.second == 1){
+//                 return it.first;
+//             }
+//         }
+//         return -1;
+//     }
+// };
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int> mpp;
+        int a = 0;
         for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
+            a = a ^ nums[i];
         }
-        for(auto it:mpp){
-            if(it.second == 1){
-                return it.first;
-            }
-        }
-        return -1;
+        return a;
     }
 };
