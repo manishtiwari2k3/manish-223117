c1afdc871be811ca53b29bc7825753a3d3947975
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-11-04 07:00:45 +0530

    Time: 2 ms (70.29%), Space: 21.3 MB (81.51%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,29 +1,47 @@
 class MinStack {
 public:
-    stack <pair<int,int>> st;
+    stack <long long> st;
+    long long mini = INT_MAX;
     MinStack() {
-        // stack <pair<int,int>> st;
+        
     }
     
     void push(int val) {
         if(st.empty()){
-            st.push({val,val});
+            st.push(val);
+            mini = val;
         }
         else{
-            st.push({val,min(val,st.top().second)});
+            if(val>mini){
+                st.push(val);
+            }
+            else{
+                st.push(2LL*val-mini);
+                mini = val;
+            }
         }
     }
     
     void pop() {
+        long long x = st.top();
         st.pop();
+        if(x<mini){
+            mini = 2*mini-x;
+        }
     }
     
     int top() {
-        return st.top().first;
+        long long x = st.top();
+        if(x<mini){
+            return mini;
+        }
+        else{
+            return x;
+        }
     }
     
     int getMin() {
-        return st.top().second;
+        return mini;
     }
 };
 
@@ -35,3 +53,42 @@ public:
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
  */
+
+
+//  class MinStack {
+// public:
+//     stack <pair<int,int>> st;
+//     MinStack() {
+//         // stack <pair<int,int>> st;
+//     }
+    
+//     void push(int val) {
+//         if(st.empty()){
+//             st.push({val,val});
+//         }
+//         else{
+//             st.push({val,min(val,st.top().second)});
+//         }
+//     }
+    
+//     void pop() {
+//         st.pop();
+//     }
+    
+//     int top() {
+//         return st.top().first;
+//     }
+    
+//     int getMin() {
+//         return st.top().second;
+//     }
+// };
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * MinStack* obj = new MinStack();
+//  * obj->push(val);
+//  * obj->pop();
+//  * int param_3 = obj->top();
+//  * int param_4 = obj->getMin();
+//  */
