6f94bc7afb0047249c958cdfae7290ef0eb1b609
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-11-07 10:48:19 +0530

    Time: 44 ms (17.91%), Space: 60.2 MB (32.98%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -20,7 +20,7 @@ public:
     int largestCombination(vector<int>& candidates) {
         int maxCount = 0;
         for (int i = 0; i < 24; i++) {
-            int count = 0;  
+            int count = 0 ;
             for (int num : candidates) {
                 if ((num & (1 << i)) != 0) {
                     count++;
