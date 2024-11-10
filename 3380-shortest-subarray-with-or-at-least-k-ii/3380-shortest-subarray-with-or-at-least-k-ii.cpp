f18f3d6b70ef6489689fe2472485a0889eab8351
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-11-10 11:13:08 +0530

    Time: 74 ms (47.25%), Space: 88.1 MB (89.97%) - LeetHub

--- 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
+++ 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
@@ -1,58 +1,101 @@
+// class Solution {
+// public:
+//     int minimumSubarrayLength(vector<int>& nums, int k) {
+//         int left = 1;
+//         int right = nums.size();
+//         int minLength = -1;
+
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+
+//             if (hasValidSubarray(nums, k, mid)) {
+//                 minLength = mid;
+//                 right = mid - 1; 
+//             } else {
+//                 left = mid + 1;  
+//             }
+//         }
+
+//         return minLength;
+//     }
+
+// public:
+//     bool hasValidSubarray(vector<int>& nums, int targetSum, int windowSize) {
+//         int arrayLength = nums.size();
+//         vector<int> bitCounts(32,0);  
+//         for (int right = 0; right < arrayLength; right++) {
+//             updateBitCounts(bitCounts, nums[right], 1);
+
+//             if (right >= windowSize) {
+//                 updateBitCounts(bitCounts, nums[right - windowSize], -1);
+//             }
+
+//             if (right >= windowSize - 1 &&
+//                 convertBitCountsToNumber(bitCounts) >= targetSum) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+
+//     void updateBitCounts(vector<int>& bitCounts, int number, int delta) {
+//         for (int bitPosition = 0; bitPosition < 32; bitPosition++) {
+//             if (((number >> bitPosition) & 1) != 0) {
+//                 bitCounts[bitPosition] += delta;
+//             }
+//         }
+//     }
+
+//     int convertBitCountsToNumber(vector<int>& bitCounts) {
+//         int number = 0;
+//         for (int bitPosition = 0; bitPosition < 32; bitPosition++) {
+//             if (bitCounts[bitPosition] != 0) {
+//                 number |= 1 << bitPosition;
+//             }
+//         }
+//         return number;
+//     }
+// };
+
+
 class Solution {
 public:
     int minimumSubarrayLength(vector<int>& nums, int k) {
-        int left = 1;
-        int right = nums.size();
-        int minLength = -1;
+        int minLength = INT_MAX;
+        int windowStart = 0;
+        int windowEnd = 0;
+        vector<int> bitCounts(32, 0); 
+        while (windowEnd < nums.size()) {
+            updateBitCounts(bitCounts, nums[windowEnd], 1);
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (hasValidSubarray(nums, k, mid)) {
-                minLength = mid;
-                right = mid - 1; 
-            } else {
-                left = mid + 1;  
+            while (windowStart <= windowEnd &&
+                   convertBitCountsToNumber(bitCounts) >= k) {
+                minLength = min(minLength, windowEnd - windowStart + 1);
+                updateBitCounts(bitCounts, nums[windowStart], -1);
+                windowStart++;
             }
+
+            windowEnd++;
         }
 
-        return minLength;
+        return minLength == INT_MAX ? -1 : minLength;
     }
-
 public:
-    bool hasValidSubarray(vector<int>& nums, int targetSum, int windowSize) {
-        int arrayLength = nums.size();
-        vector<int> bitCounts(32,0);  
-        for (int right = 0; right < arrayLength; right++) {
-            updateBitCounts(bitCounts, nums[right], 1);
-
-            if (right >= windowSize) {
-                updateBitCounts(bitCounts, nums[right - windowSize], -1);
-            }
-
-            if (right >= windowSize - 1 &&
-                convertBitCountsToNumber(bitCounts) >= targetSum) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     void updateBitCounts(vector<int>& bitCounts, int number, int delta) {
         for (int bitPosition = 0; bitPosition < 32; bitPosition++) {
-            if (((number >> bitPosition) & 1) != 0) {
+            if ((number >> bitPosition) & 1) {
                 bitCounts[bitPosition] += delta;
             }
         }
     }
 
-    int convertBitCountsToNumber(vector<int>& bitCounts) {
-        int number = 0;
+    int convertBitCountsToNumber(const vector<int>& bitCounts) {
+        int result = 0;
         for (int bitPosition = 0; bitPosition < 32; bitPosition++) {
             if (bitCounts[bitPosition] != 0) {
-                number |= 1 << bitPosition;
+                result |= 1 << bitPosition;
             }
         }
-        return number;
+        return result;
     }
 };
