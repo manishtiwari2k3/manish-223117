0df3d2922dff4fef65da1078a8ba83333403444b
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-11-11 10:19:12 +0530

    Time: 7 ms (41.97%), Space: 28.8 MB (42.81%) - LeetHub

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
@@ -1,3 +1,39 @@
+// class Solution {
+// public:
+//     bool checkPrime(int x) {
+//         for (int i = 2; i <= sqrt(x); i++) {
+//             if (x % i == 0) {
+//                 return 0;
+//             }
+//         }
+//         return 1;
+//     }
+//     bool primeSubOperation(vector<int>& nums) {
+//         for (int i = 0; i < nums.size(); i++) {
+//             int bound ;
+//             if (i == 0) {
+//                 bound = nums[0];
+//             } else {
+//                 bound = nums[i] - nums[i - 1];
+//             }
+//             if (bound <= 0) {
+//                 return 0;
+//             }
+
+//             int largestPrime = 0;
+//             for (int j = bound - 1; j >= 2; j--) {
+//                 if (checkPrime(j)) {
+//                     largestPrime = j;
+//                     break;
+//                 }
+//             }
+
+//             nums[i] = nums[i] - largestPrime;
+//         }
+//         return 1;
+//     }
+// };
+
 class Solution {
 public:
     bool checkPrime(int x) {
@@ -9,24 +45,31 @@ public:
         return 1;
     }
     bool primeSubOperation(vector<int>& nums) {
+        int maxElement = *max_element(nums.begin(), nums.end());
+
+        // Store the previousPrime array.
+        vector<int> previousPrime(maxElement + 1, 0);
+        for (int i = 2; i <= maxElement; i++) {
+            if (checkPrime(i)) {
+                previousPrime[i] = i;
+            } else {
+                previousPrime[i] = previousPrime[i - 1];
+            }
+        }
+
         for (int i = 0; i < nums.size(); i++) {
-            int bound ;
+            int bound;
             if (i == 0) {
                 bound = nums[0];
             } else {
                 bound = nums[i] - nums[i - 1];
             }
+
             if (bound <= 0) {
                 return 0;
             }
 
-            int largestPrime = 0;
-            for (int j = bound - 1; j >= 2; j--) {
-                if (checkPrime(j)) {
-                    largestPrime = j;
-                    break;
-                }
-            }
+            int largestPrime = previousPrime[bound - 1];
 
             nums[i] = nums[i] - largestPrime;
         }
