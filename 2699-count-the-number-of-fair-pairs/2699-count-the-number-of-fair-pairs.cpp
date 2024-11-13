c27167d94f08695e06ceaeb1973cb97c82e27027
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-11-13 09:40:27 +0530

    Time: 33 ms (83.93%), Space: 60.2 MB (94.04%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -1,26 +1,51 @@
+// class Solution {
+// public:
+//     long long lower_bound(vector<int>& nums, int low, int high, int element) {
+//         while (low <= high) {
+//             int mid = low + ((high - low) / 2);
+//             if (nums[mid] >= element) {
+//                 high = mid - 1;
+//             } else
+//                 low = mid + 1;
+//         }
+//         return low;
+//     }
+//     long long countFairPairs(vector<int>& nums, int lower, int upper) {
+//         sort(nums.begin(), nums.end());
+//         long long ans = 0;
+//         for (int i = 0; i < nums.size(); i++) {
+//             int low =
+//                 lower_bound(nums, i + 1, nums.size() - 1, lower - nums[i]);
+//             int high =
+//                 lower_bound(nums, i + 1, nums.size() - 1, upper - nums[i] + 1);
+
+//             ans += 1LL * (high - low);
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
-    long long lower_bound(vector<int>& nums, int low, int high, int element) {
-        while (low <= high) {
-            int mid = low + ((high - low) / 2);
-            if (nums[mid] >= element) {
-                high = mid - 1;
-            } else
-                low = mid + 1;
-        }
-        return low;
-    }
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
         sort(nums.begin(), nums.end());
-        long long ans = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int low =
-                lower_bound(nums, i + 1, nums.size() - 1, lower - nums[i]);
-            int high =
-                lower_bound(nums, i + 1, nums.size() - 1, upper - nums[i] + 1);
+        return lower_bound(nums, upper + 1) - lower_bound(nums, lower);
+    }
 
-            ans += 1LL * (high - low);
+private:
+    long long lower_bound(vector<int>& nums, int value) {
+        int left = 0, right = nums.size() - 1;
+        long long result = 0;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum < value) {
+                result += (right - left);
+                left++;
+            } else {
+                right--;
+            }
         }
-        return ans;
+        return result;
     }
 };
