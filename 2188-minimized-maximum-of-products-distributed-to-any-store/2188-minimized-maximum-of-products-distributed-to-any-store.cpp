eec7ab70e6ac8d925fb0d083187b2b2ffe8ce774
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-11-14 09:11:45 +0530

    Time: 1702 ms (5.99%), Space: 113.9 MB (12.67%) - LeetHub

--- 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,35 +1,77 @@
+// class Solution {
+// public:
+//     bool canDistribute(int x, vector<int>& quantities, int n) {
+//         int j = 0;
+//         int remaining = quantities[j];
+
+//         for (int i = 0; i < n; i++) {
+//             if (remaining <= x) {
+//                 j++;
+//                 if (j == quantities.size()) {
+//                     return true;
+//                 } else {
+//                     remaining = quantities[j];
+//                 }
+//             } else {
+//                 remaining -= x;
+//             }
+//         }
+//         return false;
+//     }
+//     int minimizedMaximum(int n, vector<int>& quantities) {
+//         int left = 0;
+//         int right = *max_element(quantities.begin(), quantities.end());
+
+//         while (left < right) {
+//             int middle = (left + right) / 2;
+//             if (canDistribute(middle, quantities, n)) {
+//                 right = middle;
+//             } else {
+//                 left = middle + 1;
+//             }
+//         }
+//         return left;
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
-    bool canDistribute(int x, vector<int>& quantities, int n) {
-        int j = 0;
-        int remaining = quantities[j];
-
-        for (int i = 0; i < n; i++) {
-            if (remaining <= x) {
-                j++;
-                if (j == quantities.size()) {
-                    return true;
-                } else {
-                    remaining = quantities[j];
-                }
-            } else {
-                remaining -= x;
-            }
-        }
-        return false;
-    }
     int minimizedMaximum(int n, vector<int>& quantities) {
-        int left = 0;
-        int right = *max_element(quantities.begin(), quantities.end());
+        int m = quantities.size();
+        auto compareTypeStorePairs = [](pair<int, int>& a, pair<int, int>& b) {
+            return (long long)a.first * b.second <
+                   (long long)a.second * b.first;
+        };
 
-        while (left < right) {
-            int middle = (left + right) / 2;
-            if (canDistribute(middle, quantities, n)) {
-                right = middle;
-            } else {
-                left = middle + 1;
-            }
+        vector<pair<int, int>> typeStorePairsArray;
+
+        for (int i = 0; i < m; i++) {
+            typeStorePairsArray.push_back({quantities[i], 1});
         }
-        return left;
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       decltype(compareTypeStorePairs)>
+            typeStorePairs(typeStorePairsArray.begin(),
+                           typeStorePairsArray.begin() + m);
+
+        for (int i = 0; i < n - m; i++) {
+            pair<int, int> pairWithMaxRatio = typeStorePairs.top();
+            int totalQuantityOfType = pairWithMaxRatio.first;
+            int storesAssignedToType = pairWithMaxRatio.second;
+            typeStorePairs.pop();
+
+            typeStorePairs.push(
+                {totalQuantityOfType, storesAssignedToType + 1});
+        }
+
+        pair<int, int> pairWithMaxRatio = typeStorePairs.top();
+        int totalQuantityOfType = pairWithMaxRatio.first;
+        int storesAssignedToType = pairWithMaxRatio.second;
+
+        return ceil((double)totalQuantityOfType / storesAssignedToType);
     }
 };
