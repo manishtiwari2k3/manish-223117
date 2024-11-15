7ca820fbf739803f1923dcea935ae8491e890ce4
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-11-15 15:19:30 +0530

    Time: 6 ms (75.59%), Space: 10.7 MB (88.02%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,43 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxlen = 0;
         int n = s.length();
-        for(int i=0;i<n;i++){
-            int hash[256] = {0};
-            for(int j=i;j<n;j++){
-                if(hash[s[j]]==1){
-                    break;
+        int l=0;
+        int r = 0;
+        int maxlen = 0;
+        int hash[256];
+        fill(begin(hash), end(hash) , -1);
+        while(r<n){
+            if(hash[s[r]]!=(-1)){
+                if(hash[s[r]]>=l){
+                    l = hash[s[r]]+1;
                 }
-                int len = j-i+1;
-                maxlen = max(len,maxlen);
-                hash[s[j]]=1;
             }
+            int len = r-l+1;
+            maxlen = max(len,maxlen);
+            hash[s[r]] =  r;
+            r++;
         }
         return maxlen;
     }
 };
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int maxlen = 0;
+//         int n = s.length();
+//         for(int i=0;i<n;i++){
+//             int hash[256] = {0};
+//             for(int j=i;j<n;j++){
+//                 if(hash[s[j]]==1){
+//                     break;
+//                 }
+//                 int len = j-i+1;
+//                 maxlen = max(len,maxlen);
+//                 hash[s[j]]=1;
+//             }
+//         }
+//         return maxlen;
+//     }
+// };
