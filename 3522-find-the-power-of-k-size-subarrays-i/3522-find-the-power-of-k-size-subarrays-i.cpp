c68073cc63c816419baafc7f7fc8a41993991da2
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-11-16 07:52:39 +0530

    Time: 0 ms (100%), Space: 32.7 MB (99.59%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
@@ -1,22 +1,56 @@
+// // class Solution {
+// // public:
+// //     vector<int> resultsArray(vector<int>& nums, int k) {
+// //         int length = nums.size();
+// //         vector<int> result(length - k + 1);
+
+// //         for (int start = 0; start <= length - k; start++) {
+// //             bool isConsecutiveAndSorted = true;
+
+// //             for (int index = start; index < start + k - 1; index++) {
+// //                 if (nums[index + 1] != nums[index] + 1) {
+// //                     isConsecutiveAndSorted = false;
+// //                     break;
+// //                 }
+// //             }
+// //             if (isConsecutiveAndSorted) {
+// //                 result[start] = nums[start + k - 1];
+// //             } else {
+// //                 result[start] = -1;
+// //             }
+// //         }
+
+// //         return result;
+// //     }
+// // };
+
+
 // class Solution {
 // public:
 //     vector<int> resultsArray(vector<int>& nums, int k) {
 //         int length = nums.size();
-//         vector<int> result(length - k + 1);
+//         vector<int> result(length - k + 1, -1);
+//         deque<int> indexDeque;
 
-//         for (int start = 0; start <= length - k; start++) {
-//             bool isConsecutiveAndSorted = true;
-
-//             for (int index = start; index < start + k - 1; index++) {
-//                 if (nums[index + 1] != nums[index] + 1) {
-//                     isConsecutiveAndSorted = false;
-//                     break;
-//                 }
+//         for (int currentIndex = 0; currentIndex < length; currentIndex++) {
+//             if (!indexDeque.empty() &&
+//                 indexDeque.front() < currentIndex - k + 1) {
+//                 indexDeque.pop_front();
 //             }
-//             if (isConsecutiveAndSorted) {
-//                 result[start] = nums[start + k - 1];
-//             } else {
-//                 result[start] = -1;
+
+//             if (!indexDeque.empty() &&
+//                 nums[currentIndex] != nums[currentIndex - 1] + 1) {
+//                 indexDeque.clear();
+//             }
+
+//             indexDeque.push_back(currentIndex);
+
+//             if (currentIndex >= k - 1) {
+//                 if (indexDeque.size() == k) {
+//                     result[currentIndex - k + 1] = nums[indexDeque.back()];
+//                 } else {
+//                     result[currentIndex - k + 1] = -1;
+//                 }
 //             }
 //         }
 
@@ -28,29 +62,22 @@
 class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
-        int length = nums.size();
-        vector<int> result(length - k + 1, -1);
-        deque<int> indexDeque;
+        if (k == 1) {
+            return nums;
+        }
 
-        for (int currentIndex = 0; currentIndex < length; currentIndex++) {
-            if (!indexDeque.empty() &&
-                indexDeque.front() < currentIndex - k + 1) {
-                indexDeque.pop_front();
+        size_t length = nums.size();
+        vector<int> result(length - k + 1, -1);  
+        int consecutiveCount = 1;
+
+        for (size_t index = 0; index < length - 1; index++) {
+            if (nums[index] + 1 == nums[index + 1]) {
+                consecutiveCount++;
+            } else {
+                consecutiveCount = 1;  
             }
-
-            if (!indexDeque.empty() &&
-                nums[currentIndex] != nums[currentIndex - 1] + 1) {
-                indexDeque.clear();
-            }
-
-            indexDeque.push_back(currentIndex);
-
-            if (currentIndex >= k - 1) {
-                if (indexDeque.size() == k) {
-                    result[currentIndex - k + 1] = nums[indexDeque.back()];
-                } else {
-                    result[currentIndex - k + 1] = -1;
-                }
+            if (consecutiveCount >= k) {
+                result[index - k + 2] = nums[index + 1];
             }
         }
 
