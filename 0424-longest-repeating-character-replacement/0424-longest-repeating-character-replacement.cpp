1f30e678d7d42267d209c047d5260c5626b8851a
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-11-22 13:47:38 +0530

    Time: 4 ms (61.49%), Space: 10 MB (8.11%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l = 0 , r = 0 , maxlen = 0, maxf = 0;
+        int hash[26] = {0};
+        while(r<s.size()){
+            hash[s[r]-'A']++;
+            maxf = max(maxf,hash[s[r]-'A']);
+            while((r-l+1)-maxf>k){
+                hash[s[l]-'A']--;
+                maxf = 0;
+                for(int i=0;i<26;i++){
+                    maxf = max(maxf,hash[i]);
+                }
+                l++;
+            }
+            if((r-l+1)-maxf<=k){
+                maxlen= max(maxlen,r-l+1);
+            }
+            r++;
+        }
+        return maxlen;
+    }
+};
+
+
+// class Solution {
+// public:
+//     int characterReplacement(string s, int k) {
+//         int maxlen = 0;
+//         for(int i=0;i<s.length();i++){
+//             int hash[26] = {0};
+//             int maxf = 0;
+//             for(int j=i;j<s.length();j++){
+//                 hash[s[j]-'A']++;
+//                 maxf = max(maxf,hash[s[j]-'A']);
+//                 int changes = (j-i+1)-maxf;
+//                 if(changes<=k){
+//                     maxlen = max(maxlen,j-i+1);
+//                 }
+//                 else{
+//                     break;
+//                 }
+//             }
+//         }
+//         return maxlen;
+//     }
+// };
