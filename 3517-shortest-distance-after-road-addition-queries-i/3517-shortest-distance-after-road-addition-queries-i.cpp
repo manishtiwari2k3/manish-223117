0d3352fafce3e9f2d4262d46fbcba1d14a7efdc2
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-11-27 21:29:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
+++ 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
@@ -11,7 +11,7 @@ public:
         int layersExplored = 0;
 
         while (!nodeQueue.empty()) {
-            for (int i = 0; i < currentLayerNodeCount; ++i) {
+            for (int i = 1; i < currentLayerNodeCount; ++i) {
                 int currentNode = nodeQueue.front();
                 nodeQueue.pop();
 
