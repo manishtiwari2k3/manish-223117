3f4d8aeeb2443ea2f88671770f418f43bd003043
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-11-27 21:31:23 +0530

    Time: 91 ms (75.24%), Space: 52.7 MB (92.2%) - LeetHub

--- 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
+++ 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
@@ -1,57 +1,101 @@
+// class Solution {
+// public:
+//     int bfs(int n, vector<vector<int>>& adjList) {
+//         vector<bool> visited(n, false);
+//         queue<int> nodeQueue;
+//         nodeQueue.push(0);
+//         visited[0] = true;
+
+//         int currentLayerNodeCount = 1;
+//         int nextLayerNodeCount = 0;
+//         int layersExplored = 0;
+
+//         while (!nodeQueue.empty()) {
+//             for (int i = 0; i < currentLayerNodeCount; ++i) {
+//                 int currentNode = nodeQueue.front();
+//                 nodeQueue.pop();
+
+//                 if (currentNode == n - 1) {
+//                     return layersExplored;  
+//                 }
+
+//                 for (auto neighbor : adjList[currentNode]) {
+//                     if (visited[neighbor]) continue;
+//                     nodeQueue.push(
+//                         neighbor);  
+//                     nextLayerNodeCount++;  
+//                     visited[neighbor] = true;
+//                 }
+//             }
+
+//             currentLayerNodeCount = nextLayerNodeCount;
+//             nextLayerNodeCount = 0;
+//             layersExplored++;  
+//         }
+
+//         return -1;  
+//     }
+
+//     vector<int> shortestDistanceAfterQueries(int n,
+//                                              vector<vector<int>>& queries) {
+//         vector<int> answer;
+//         vector<vector<int>> adjList(n, vector<int>(0));
+
+//         for (int i = 0; i < n - 1; i++) {
+//             adjList[i].push_back(i + 1);
+//         }
+//         for (auto& road : queries) {
+//             int u = road[0];
+//             int v = road[1];
+//             adjList[u].push_back(v); 
+//             answer.push_back(bfs(n, adjList));
+//         }
+
+//         return answer;
+//     }
+// };
+
 class Solution {
 public:
-    int bfs(int n, vector<vector<int>>& adjList) {
-        vector<bool> visited(n, false);
-        queue<int> nodeQueue;
-        nodeQueue.push(0);
-        visited[0] = true;
+    int findMinDistance(vector<vector<int>> &adjList, int n, int currentNode,
+                        vector<int> &dp) {
+        if (currentNode == n - 1) return 0;
 
-        int currentLayerNodeCount = 1;
-        int nextLayerNodeCount = 0;
-        int layersExplored = 0;
+        if (dp[currentNode] != -1) return dp[currentNode];
 
-        while (!nodeQueue.empty()) {
-            for (int i = 0; i < currentLayerNodeCount; ++i) {
-                int currentNode = nodeQueue.front();
-                nodeQueue.pop();
+        int minDistance = n;
 
-                if (currentNode == n - 1) {
-                    return layersExplored;  
-                }
-
-                for (auto neighbor : adjList[currentNode]) {
-                    if (visited[neighbor]) continue;
-                    nodeQueue.push(
-                        neighbor);  
-                    nextLayerNodeCount++;  
-                    visited[neighbor] = true;
-                }
-            }
-
-            currentLayerNodeCount = nextLayerNodeCount;
-            nextLayerNodeCount = 0;
-            layersExplored++;  
+        for (int neighbor : adjList[currentNode]) {
+            minDistance =
+                min(minDistance, findMinDistance(adjList, n, neighbor, dp) + 1);
         }
 
-        return -1;  
+        return dp[currentNode] = minDistance;
     }
 
     vector<int> shortestDistanceAfterQueries(int n,
-                                             vector<vector<int>>& queries) {
-        vector<int> answer;
+                                             vector<vector<int>> &queries) {
+        vector<int> dp(
+            n, -1); 
         vector<vector<int>> adjList(n, vector<int>(0));
 
         for (int i = 0; i < n - 1; i++) {
             adjList[i].push_back(i + 1);
         }
-        for (auto& road : queries) {
+
+        vector<int> answer;
+
+        for (auto &road : queries) {
             int u = road[0];
             int v = road[1];
-            adjList[u].push_back(v); 
-            answer.push_back(bfs(n, adjList));
+
+            adjList[u].push_back(v);
+            answer.push_back(findMinDistance(adjList, n, 0, dp));
+
+            dp.clear();
+            dp.resize(n, -1);
         }
 
-        return answer;
+        return answer; 
     }
 };
-
