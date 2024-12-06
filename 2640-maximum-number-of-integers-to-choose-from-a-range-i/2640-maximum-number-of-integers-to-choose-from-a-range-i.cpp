2ff96e8fcaf78399d93d86e8452e935183cd61eb
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-12-06 20:40:58 +0530

    Time: 47 ms (90.84%), Space: 117.7 MB (82.66%) - LeetHub

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,38 +1,62 @@
+// class Solution {
+// public:
+//     int maxCount(vector<int>& banned, int n, int maxSum) {
+//         sort(banned.begin(), banned.end());
+
+//         int count = 0;
+
+//         for (int num = 1; num <= n; num++) {
+//             if (customBinarySearch(banned, num)) continue;
+
+//             maxSum -= num;
+
+//             if (maxSum < 0) break;
+
+//             count++;
+//         }
+//         return count;
+//     }
+
+// public:
+//     bool customBinarySearch(vector<int>& arr, int target) {
+//         int left = 0;
+//         int right = arr.size() - 1;
+
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+
+//             if (arr[mid] == target) return true;
+
+//             if (arr[mid] > target) {
+//                 right = mid - 1;
+//             } else {
+//                 left = mid + 1;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
 class Solution {
 public:
     int maxCount(vector<int>& banned, int n, int maxSum) {
         sort(banned.begin(), banned.end());
 
-        int count = 0;
+        int bannedIdx = 0, count = 0;
 
-        for (int num = 1; num <= n; num++) {
-            if (customBinarySearch(banned, num)) continue;
-
-            maxSum -= num;
-
-            if (maxSum < 0) break;
-
-            count++;
-        }
-        return count;
-    }
-
-public:
-    bool customBinarySearch(vector<int>& arr, int target) {
-        int left = 0;
-        int right = arr.size() - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] == target) return true;
-
-            if (arr[mid] > target) {
-                right = mid - 1;
+        for (int num = 1; num <= n && maxSum >= 0; num++) {
+            if (bannedIdx < banned.size() && banned[bannedIdx] == num) {
+                while (bannedIdx < banned.size() && banned[bannedIdx] == num) {
+                    bannedIdx++;
+                }
             } else {
-                left = mid + 1;
+                maxSum -= num;
+                if (maxSum >= 0) {
+                    count++;
+                }
             }
         }
-        return false;
+        return count;
     }
 };
