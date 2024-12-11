b6d06a29b81c46d02a5cca7381a122007e1686f3
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-12-11 19:07:19 +0530

    Time: 54 ms (55.89%), Space: 105.1 MB (67.7%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,29 +1,47 @@
+// class Solution {
+// public:
+//     int maximumBeauty(vector<int>& nums, int k) {
+//         sort(nums.begin(), nums.end());
+//         int maxBeauty = 0;
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             int upperBound = findUpperBound(nums, nums[i] + 2 * k);
+//             maxBeauty = max(maxBeauty, upperBound - i + 1);
+//         }
+//         return maxBeauty;
+//     }
+
+// public:
+//     int findUpperBound(vector<int>& arr, int val) {
+//         int low = 0, high = arr.size() - 1, result = 0;
+
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             if (arr[mid] <= val) {
+//                 result = mid; 
+//                 low = mid + 1;
+//             } else {
+//                 high = mid - 1;  
+//             }
+//         }
+//         return result;
+//     }
+// };
+
+
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
+        int right = 0;
         int maxBeauty = 0;
 
-        for (int i = 0; i < nums.size(); i++) {
-            int upperBound = findUpperBound(nums, nums[i] + 2 * k);
-            maxBeauty = max(maxBeauty, upperBound - i + 1);
+        for (int left = 0; left < nums.size(); left++) {
+            while (right < nums.size() && nums[right] - nums[left] <= 2 * k) {
+                right++;
+            }
+            maxBeauty = max(maxBeauty, right - left);
         }
         return maxBeauty;
-    }
-
-public:
-    int findUpperBound(vector<int>& arr, int val) {
-        int low = 0, high = arr.size() - 1, result = 0;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] <= val) {
-                result = mid; 
-                low = mid + 1;
-            } else {
-                high = mid - 1;  
-            }
-        }
-        return result;
     }
 };
