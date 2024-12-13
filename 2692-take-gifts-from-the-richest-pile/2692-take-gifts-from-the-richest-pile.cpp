cebd08e9a721e98ec121e47b30e0a1e4a5829ba1
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-12-13 13:11:53 +0530

    Time: 3 ms (54.35%), Space: 13.5 MB (8.11%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -1,22 +1,47 @@
+// class Solution {
+// public:
+//     long long pickGifts(vector<int>& gifts, int k) {
+//         int n = gifts.size();
+
+//         for (int i = 0; i < k; i++) {
+//             int richestPileIndex = 0;
+//             for (int currentPileIndex = 0; currentPileIndex < n;
+//                  currentPileIndex++) {
+//                 if (gifts[richestPileIndex] < gifts[currentPileIndex]) {
+//                     richestPileIndex = currentPileIndex;
+//                 }
+//             }
+
+//             gifts[richestPileIndex] = sqrt(gifts[richestPileIndex]);
+//         }
+
+//         long long int numberOfRemainingGifts =
+//             accumulate(gifts.begin(), gifts.end(), 0LL);
+
+//         return numberOfRemainingGifts;
+//     }
+// };
+
 class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
         int n = gifts.size();
 
-        for (int i = 0; i < k; i++) {
-            int richestPileIndex = 0;
-            for (int currentPileIndex = 0; currentPileIndex < n;
-                 currentPileIndex++) {
-                if (gifts[richestPileIndex] < gifts[currentPileIndex]) {
-                    richestPileIndex = currentPileIndex;
-                }
-            }
+        vector<int> sortedGifts = gifts;
+        sort(sortedGifts.begin(), sortedGifts.end());
 
-            gifts[richestPileIndex] = sqrt(gifts[richestPileIndex]);
+        for (int i = 0; i < k; i++) {
+            int maxElement = sortedGifts[n - 1];
+            sortedGifts.pop_back();
+
+            auto spotOfSqrt = upper_bound(sortedGifts.begin(),
+                                          sortedGifts.end(), sqrt(maxElement));
+
+            sortedGifts.emplace(spotOfSqrt, sqrt(maxElement));
         }
 
         long long int numberOfRemainingGifts =
-            accumulate(gifts.begin(), gifts.end(), 0LL);
+            accumulate(sortedGifts.begin(), sortedGifts.end(), 0LL);
 
         return numberOfRemainingGifts;
     }
