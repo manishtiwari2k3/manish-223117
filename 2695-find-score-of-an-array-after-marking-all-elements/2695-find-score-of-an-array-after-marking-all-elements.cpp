76baff8d94b2aded95481f61f6938dfc86880575
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-12-13 13:09:30 +0530

    Time: 118 ms (71.12%), Space: 105.5 MB (69.61%) - LeetHub

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
@@ -1,23 +1,65 @@
+// class Solution {
+// public:
+//     long findScore(vector<int>& nums) {
+//         long ans = 0;
+//         vector<pair<int, int>> sorted(nums.size());
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             sorted[i] = make_pair(nums[i], i);
+//         }
+
+//         sort(sorted.begin(), sorted.end());
+
+//         vector<bool> marked(nums.size(), false);
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             int number = sorted[i].first;
+//             int index = sorted[i].second;
+//             if (!marked[index]) {
+//                 ans += number;
+//                 marked[index] = true;
+//                 if (index - 1 >= 0) {
+//                     marked[index - 1] = true;
+//                 }
+//                 if (index + 1 < nums.size()) {
+//                     marked[index + 1] = true;
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
-    long findScore(vector<int>& nums) {
-        long ans = 0;
-        vector<pair<int, int>> sorted(nums.size());
+    long long findScore(vector<int>& nums) {
+        long long ans = 0;
+        vector<bool> marked(nums.size());
 
-        for (int i = 0; i < nums.size(); i++) {
-            sorted[i] = make_pair(nums[i], i);
+        auto cmp = [](pair<int, int>& pair1, pair<int, int>& pair2) {
+            if (pair1.first != pair2.first) return pair1.first > pair2.first;
+            return pair1.second > pair2.second;
+        };
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)>
+            heap(cmp);
+
+        for (int i = 0; i < nums.size(); ++i) {
+            heap.push(make_pair(nums[i], i));
         }
 
-        sort(sorted.begin(), sorted.end());
+        while (!heap.empty()) {
+            pair<int, int> element = heap.top();
+            heap.pop();
+            int number = element.first;
+            int index = element.second;
 
-        vector<bool> marked(nums.size(), false);
-
-        for (int i = 0; i < nums.size(); i++) {
-            int number = sorted[i].first;
-            int index = sorted[i].second;
             if (!marked[index]) {
                 ans += number;
                 marked[index] = true;
+
                 if (index - 1 >= 0) {
                     marked[index - 1] = true;
                 }
