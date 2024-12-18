1fe1fd677f17fe444d1463b2e36f65bc865c7cb2
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-12-18 20:01:47 +0530

    Time: 3 ms (16%), Space: 14.2 MB (9.86%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -21,19 +21,15 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        // Create a copy of prices array to store discounted prices
         vector<int> result = prices;
 
         stack<int> stack;
 
         for (int i = 0; i < prices.size(); i++) {
-            // Process items that can be discounted by current price
             while (!stack.empty() && prices[stack.top()] >= prices[i]) {
-                // Apply discount to previous item using current price
                 result[stack.top()] -= prices[i];
                 stack.pop();
             }
-            // Add current index to stack
             stack.push(i);
         }
 
