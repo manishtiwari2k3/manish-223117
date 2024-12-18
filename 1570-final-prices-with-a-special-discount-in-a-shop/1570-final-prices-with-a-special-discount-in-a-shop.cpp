5fbda794d20c4eb6c1239ef9655538bb3ca20aa2
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-12-18 20:01:23 +0530

    Time: 0 ms (100%), Space: 14.2 MB (17.75%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,21 +1,40 @@
+// class Solution {
+// public:
+//     vector<int> finalPrices(vector<int>& prices) {
+//         int n = prices.size();
+//         vector<int> result = prices;
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 if (prices[j] <= prices[i]) {
+//                     result[i] = prices[i] - prices[j];
+//                     break;
+//                 }
+//             }
+//         }
+
+//         return result;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        int n = prices.size();
-        // Create a copy of original prices array to store discounted prices
+        // Create a copy of prices array to store discounted prices
         vector<int> result = prices;
 
-        for (int i = 0; i < n; i++) {
-            // Look for first smaller or equal price that comes after current
-            // item
-            for (int j = i + 1; j < n; j++) {
-                if (prices[j] <= prices[i]) {
-                    // Apply discount by subtracting prices[j] from current
-                    // price
-                    result[i] = prices[i] - prices[j];
-                    break;
-                }
+        stack<int> stack;
+
+        for (int i = 0; i < prices.size(); i++) {
+            // Process items that can be discounted by current price
+            while (!stack.empty() && prices[stack.top()] >= prices[i]) {
+                // Apply discount to previous item using current price
+                result[stack.top()] -= prices[i];
+                stack.pop();
             }
+            // Add current index to stack
+            stack.push(i);
         }
 
         return result;
