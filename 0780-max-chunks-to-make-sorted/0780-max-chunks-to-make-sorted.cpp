645fb6b4c94bc7ab4f8618fef40ea42138bc214e
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-12-19 16:02:56 +0530

    Time: 0 ms (100%), Space: 9.1 MB (42.82%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -29,15 +29,10 @@ public:
         int n = arr.size();
         int chunks = 0, prefixSum = 0, sortedPrefixSum = 0;
 
-        // Iterate over the array
         for (int i = 0; i < n; i++) {
-            // Update prefix sum of `arr`
             prefixSum += arr[i];
-            // Update prefix sum of the sorted array
             sortedPrefixSum += i;
 
-            // If the two sums are equal, the two prefixes contain the same
-            // elements; a chunk can be formed
             if (prefixSum == sortedPrefixSum) {
                 chunks++;
             }
