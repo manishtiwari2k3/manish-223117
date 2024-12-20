918cc8d27064e61838bab3cc1c1c5bd56ff00ed9
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-12-20 11:33:30 +0530

    Time: 4 ms (42.65%), Space: 84.1 MB (13.13%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
@@ -1,22 +1,67 @@
+// class Solution {
+// public:
+//     TreeNode* reverseOddLevels(TreeNode* root) {
+//         traverseDFS(root->left, root->right, 0);
+//         return root;
+//     }
+
+// public:
+//     void traverseDFS(TreeNode* leftChild, TreeNode* rightChild, int level) {
+//         if (leftChild == nullptr || rightChild == nullptr) {
+//             return;
+//         }
+//         if (level % 2 == 0) {
+//             int temp = leftChild->val;
+//             leftChild->val = rightChild->val;
+//             rightChild->val = temp;
+//         }
+
+//         traverseDFS(leftChild->left, rightChild->right, level + 1);
+//         traverseDFS(leftChild->right, rightChild->left, level + 1);
+//     }
+// };
+
 class Solution {
 public:
     TreeNode* reverseOddLevels(TreeNode* root) {
-        traverseDFS(root->left, root->right, 0);
-        return root;
-    }
-
-public:
-    void traverseDFS(TreeNode* leftChild, TreeNode* rightChild, int level) {
-        if (leftChild == nullptr || rightChild == nullptr) {
-            return;
-        }
-        if (level % 2 == 0) {
-            int temp = leftChild->val;
-            leftChild->val = rightChild->val;
-            rightChild->val = temp;
+        if (!root) {
+            return nullptr;  // Return null if the tree is empty.
         }
 
-        traverseDFS(leftChild->left, rightChild->right, level + 1);
-        traverseDFS(leftChild->right, rightChild->left, level + 1);
+        queue<TreeNode*> q;
+        q.push(root);  // Start BFS with the root node.
+        int level = 0;
+
+        while (!q.empty()) {
+            int size = q.size();
+            vector<TreeNode*> currentLevelNodes;
+
+            // Process all nodes at the current level.
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                currentLevelNodes.push_back(node);
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            // Reverse node values if the current level is odd.
+            if (level % 2 == 1) {
+                int left = 0, right = currentLevelNodes.size() - 1;
+                while (left < right) {
+                    int temp = currentLevelNodes[left]->val;
+                    currentLevelNodes[left]->val =
+                        currentLevelNodes[right]->val;
+                    currentLevelNodes[right]->val = temp;
+                    left++;
+                    right--;
+                }
+            }
+
+            level++;
+        }
+
+        return root;  // Return the modified tree root.
     }
 };
