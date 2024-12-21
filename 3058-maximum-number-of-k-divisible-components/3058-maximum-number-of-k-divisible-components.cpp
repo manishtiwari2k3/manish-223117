99fa43db4f9b9a0daa35658139395ee232d50b90
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-12-21 13:05:42 +0530

    Time: 115 ms (63.92%), Space: 176 MB (68.99%) - LeetHub

--- 3058-maximum-number-of-k-divisible-components/3058-maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/3058-maximum-number-of-k-divisible-components.cpp
@@ -1,39 +1,92 @@
+// // class Solution {
+// // public:
+// //     int maxKDivisibleComponents(int n, vector<vector<int>> &edges,
+// //                                 vector<int> &values, int k) {
+// //         vector<int> adjList[n];
+// //         for (auto edge : edges) {
+// //             int node1 = edge[0];
+// //             int node2 = edge[1];
+// //             adjList[node1].push_back(node2);
+// //             adjList[node2].push_back(node1);
+// //         }
+// //         int componentCount = 0;
+// //         dfs(0, -1, adjList, values, k, componentCount);
+
+// //         return componentCount;
+// //     }
+
+// // private:
+// //     int dfs(int currentNode, int parentNode, vector<int> adjList[],
+// //             vector<int> &nodeValues, int k, int &componentCount) {
+// //         int sum = 0;
+
+// //         for (auto neighborNode : adjList[currentNode]) {
+// //             if (neighborNode != parentNode) {
+// //                 sum += dfs(neighborNode, currentNode, adjList, nodeValues, k,
+// //                            componentCount);
+// //                 sum %= k; 
+// //             }
+// //         }
+
+// //         sum += nodeValues[currentNode];
+
+// //         sum %= k;
+// //         if (sum == 0) componentCount++;
+
+// //         return sum;
+// //     }
+// // };
+
+
 // class Solution {
 // public:
-//     int maxKDivisibleComponents(int n, vector<vector<int>> &edges,
-//                                 vector<int> &values, int k) {
-//         vector<int> adjList[n];
-//         for (auto edge : edges) {
-//             int node1 = edge[0];
-//             int node2 = edge[1];
-//             adjList[node1].push_back(node2);
-//             adjList[node2].push_back(node1);
-//         }
+//     int maxKDivisibleComponents(int n, vector<vector<int>>& edges,
+//                                 vector<int>& values, int k) {
+//         if (n < 2) return 1;
+
 //         int componentCount = 0;
-//         dfs(0, -1, adjList, values, k, componentCount);
+//         unordered_map<int, unordered_set<int>> graph;
 
-//         return componentCount;
-//     }
+//         for (const auto& edge : edges) {
+//             int node1 = edge[0], node2 = edge[1];
+//             graph[node1].insert(node2);
+//             graph[node2].insert(node1);
+//         }
 
-// private:
-//     int dfs(int currentNode, int parentNode, vector<int> adjList[],
-//             vector<int> &nodeValues, int k, int &componentCount) {
-//         int sum = 0;
+//         vector<long long> longValues(values.begin(), values.end());
 
-//         for (auto neighborNode : adjList[currentNode]) {
-//             if (neighborNode != parentNode) {
-//                 sum += dfs(neighborNode, currentNode, adjList, nodeValues, k,
-//                            componentCount);
-//                 sum %= k; 
+//         queue<int> queue;
+//         for (const auto& [node, neighbors] : graph) {
+//             if (neighbors.size() == 1) {
+//                 queue.push(node);
 //             }
 //         }
 
-//         sum += nodeValues[currentNode];
+//         while (!queue.empty()) {
+//             int currentNode = queue.front();
+//             queue.pop();
 
-//         sum %= k;
-//         if (sum == 0) componentCount++;
+//             int neighborNode = -1;
+//             if (!graph[currentNode].empty()) {
+//                 neighborNode = *graph[currentNode].begin();
+//             }
 
-//         return sum;
+//             if (neighborNode >= 0) {
+//                 graph[neighborNode].erase(currentNode);
+//                 graph[currentNode].erase(neighborNode);
+//             }
+
+//             if (longValues[currentNode] % k == 0) {
+//                 componentCount++;
+//             } else if (neighborNode >= 0) {
+//                 longValues[neighborNode] += longValues[currentNode];
+//             }
+//             if (neighborNode >= 0 && graph[neighborNode].size() == 1) {
+//                 queue.push(neighborNode);
+//             }
+//         }
+
+//         return componentCount;
 //     }
 // };
 
@@ -45,19 +98,22 @@ public:
         if (n < 2) return 1;
 
         int componentCount = 0;
-        unordered_map<int, unordered_set<int>> graph;
+        vector<vector<int>> graph(n);
+        vector<int> inDegree(n, 0);
 
         for (const auto& edge : edges) {
             int node1 = edge[0], node2 = edge[1];
-            graph[node1].insert(node2);
-            graph[node2].insert(node1);
+            graph[node1].push_back(node2);
+            graph[node2].push_back(node1);
+            inDegree[node1]++;
+            inDegree[node2]++;
         }
 
         vector<long long> longValues(values.begin(), values.end());
 
         queue<int> queue;
-        for (const auto& [node, neighbors] : graph) {
-            if (neighbors.size() == 1) {
+        for (int node = 0; node < n; node++) {
+            if (inDegree[node] == 1) {
                 queue.push(node);
             }
         }
@@ -65,24 +121,27 @@ public:
         while (!queue.empty()) {
             int currentNode = queue.front();
             queue.pop();
+            inDegree[currentNode]--;
 
-            int neighborNode = -1;
-            if (!graph[currentNode].empty()) {
-                neighborNode = *graph[currentNode].begin();
-            }
-
-            if (neighborNode >= 0) {
-                graph[neighborNode].erase(currentNode);
-                graph[currentNode].erase(neighborNode);
-            }
+            long long addValue = 0;
 
             if (longValues[currentNode] % k == 0) {
                 componentCount++;
-            } else if (neighborNode >= 0) {
-                longValues[neighborNode] += longValues[currentNode];
+            } else {
+                addValue = longValues[currentNode];
             }
-            if (neighborNode >= 0 && graph[neighborNode].size() == 1) {
-                queue.push(neighborNode);
+
+            for (int neighborNode : graph[currentNode]) {
+                if (inDegree[neighborNode] == 0) {
+                    continue;
+                }
+
+                inDegree[neighborNode]--;
+                longValues[neighborNode] += addValue;
+
+                if (inDegree[neighborNode] == 1) {
+                    queue.push(neighborNode);
+                }
             }
         }
 
