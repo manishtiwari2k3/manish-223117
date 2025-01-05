0468feea95627d5e0b2c81652bcaa75e1739ab86
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-01-05 20:15:31 +0530

    Time: 53 ms (24.17%), Space: 108.7 MB (16.18%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
@@ -3,24 +3,20 @@ public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
         int n = s.size();
         vector<int> diffArray(
-            n, 0);  // Initialize a difference array with all elements set to 0.
+            n, 0); 
 
         for (auto shift : shifts) {
-            if (shift[2] == 1) {        // If direction is forward (1):
-                diffArray[shift[0]]++;  // Increment at the start index to
-                                        // indicate a forward shift.
+            if (shift[2] == 1) {      
+                diffArray[shift[0]]++;  
                 if (shift[1] + 1 < n) {
                     diffArray[shift[1] +
-                              1]--;  // Decrement at the end+1 index to stop the
-                                     // effect of the shift.
+                              1]--; 
                 }
-            } else {                    // If direction is backward (0):
-                diffArray[shift[0]]--;  // Decrement at the start index to
-                                        // indicate a backward shift.
+            } else {                    
+                diffArray[shift[0]]--; 
                 if (shift[1] + 1 < n) {
                     diffArray[shift[1] +
-                              1]++;  // Increment at the end+1 index to stop the
-                                     // effect of the shift.
+                              1]++;  
                 }
             }
         }
@@ -30,13 +26,11 @@ public:
 
         for (int i = 0; i < s.size(); i++) {
             numberOfShifts = (numberOfShifts + diffArray[i]) %
-                             26;  // Update cumulative shifts, keeping within
-                                  // the alphabet range.
+                             26;  
             if (numberOfShifts < 0)
                 numberOfShifts +=
-                    26;  // Ensure `numberOfShifts` is non-negative.
+                    26;  
 
-            // Calculate the new character by shifting `s[i]`
             result[i] = 'a' + (s[i] - 'a' + numberOfShifts) % 26;
         }
 
