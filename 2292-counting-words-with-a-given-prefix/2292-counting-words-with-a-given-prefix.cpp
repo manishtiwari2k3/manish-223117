988c4108311f56080bb1f2333119c49b4870d664
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-01-09 13:31:44 +0530

    Time: 0 ms (100%), Space: 12.9 MB (95.47%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
@@ -1,25 +1,38 @@
+// class Solution {
+// public:
+//     int prefixCount(vector<string>& words, string pref) {
+//         int count = 0;
+//         for (string& word : words) {
+//             count += hasPrefix(word, pref);
+//         }
+//         return count;
+//     }
+
+// public:
+//     int hasPrefix(string& str, string& pref) {
+//         int itr;
+//         for (itr = 0; itr < str.length() && itr < pref.length(); itr++) {
+//             if (str[itr] != pref[itr]) {
+//                 return 0; 
+//             }
+//         }
+
+//         if (itr != pref.length()) {
+//             return 0;  
+//         }
+//         return 1;
+//     }
+// };
+
 class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) {
         int count = 0;
         for (string& word : words) {
-            count += hasPrefix(word, pref);
-        }
-        return count;
-    }
-
-private:
-    int hasPrefix(string& str, string& pref) {
-        int itr;
-        for (itr = 0; itr < str.length() && itr < pref.length(); itr++) {
-            if (str[itr] != pref[itr]) {
-                return 0; 
+            if (word.find(pref) == 0) {
+                count++;  // Found a string with pref as prefix
             }
         }
-
-        if (itr != pref.length()) {
-            return 0;  
-        }
-        return 1;
+        return count;
     }
 };
