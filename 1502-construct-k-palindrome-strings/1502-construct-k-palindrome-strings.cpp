044b671625e9c3e873111d55cd74072c1eee18f0
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-01-11 23:52:25 +0530

    Time: 0 ms (100%), Space: 14.8 MB (78.67%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
@@ -1,20 +1,44 @@
+// class Solution {
+// public:
+//     bool canConstruct(string s, int k) {
+//         if (s.length() < k) return false;
+//         if (s.length() == k) return true;
+
+//         vector<int> freq(26);
+//         int oddCount = 0;
+
+//         for (auto& chr : s) freq[chr - 'a']++;
+
+//         for (int i = 0; i < 26; i++) {
+//             if (freq[i] % 2 == 1) {
+//                 oddCount++;
+//             }
+//         }
+
+//         return (oddCount <= k);
+//     }
+// };
+
+
 class Solution {
 public:
     bool canConstruct(string s, int k) {
+        // Handle edge cases
         if (s.length() < k) return false;
         if (s.length() == k) return true;
 
-        vector<int> freq(26);
+        // Initialize oddCount as an integer bitmask
         int oddCount = 0;
 
-        for (auto& chr : s) freq[chr - 'a']++;
-
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] % 2 == 1) {
-                oddCount++;
-            }
+        // Update the bitmask for each character in the string
+        for (char chr : s) {
+            oddCount ^= 1 << (chr - 'a');
         }
 
-        return (oddCount <= k);
+        // Count the number of set bits in the bitmask
+        int setBits = __builtin_popcount(oddCount);
+
+        // Return if the number of odd frequencies is less than or equal to k
+        return setBits <= k;
     }
 };
