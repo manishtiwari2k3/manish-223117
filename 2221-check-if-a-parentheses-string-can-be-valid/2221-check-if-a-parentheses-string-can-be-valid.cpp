0757e877f4781598ead8f5f8ed2574827435ebff
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-01-13 00:00:19 +0530

    Time: 9 ms (85.79%), Space: 29.9 MB (83.5%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,36 +1,90 @@
+// class Solution {
+// public:
+//     bool canBeValid(string s, string locked) {
+//         int length = s.size();
+//         if (length % 2 == 1) {
+//             return false;
+//         }
+
+//         stack<int> openBrackets, unlocked;
+
+//         for (int i = 0; i < length; i++) {
+//             if (locked[i] == '0') {
+//                 unlocked.push(i);
+//             } else if (s[i] == '(') {
+//                 openBrackets.push(i);
+//             } else if (s[i] == ')') {
+//                 if (!openBrackets.empty()) {
+//                     openBrackets.pop();
+//                 } else if (!unlocked.empty()) {
+//                     unlocked.pop();
+//                 } else {
+//                     return false;
+//                 }
+//             }
+//         }
+
+//         while (!openBrackets.empty() && !unlocked.empty() &&
+//                openBrackets.top() < unlocked.top()) {
+//             openBrackets.pop();
+//             unlocked.pop();
+//         }
+
+//         if (!openBrackets.empty()) {
+//             return false;
+//         }
+
+//         return true;
+//     }
+// };
+
+
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
         int length = s.size();
+        // If length of string is odd, return false.
         if (length % 2 == 1) {
             return false;
         }
-
-        stack<int> openBrackets, unlocked;
-
+        int openBrackets = 0, unlocked = 0;
+        // Iterate through the string to handle '(' and ')'.
         for (int i = 0; i < length; i++) {
             if (locked[i] == '0') {
-                unlocked.push(i);
+                unlocked++;
             } else if (s[i] == '(') {
-                openBrackets.push(i);
+                openBrackets++;
             } else if (s[i] == ')') {
-                if (!openBrackets.empty()) {
-                    openBrackets.pop();
-                } else if (!unlocked.empty()) {
-                    unlocked.pop();
+                if (openBrackets > 0) {
+                    openBrackets--;
+                } else if (unlocked > 0) {
+                    unlocked--;
                 } else {
                     return false;
                 }
             }
         }
-
-        while (!openBrackets.empty() && !unlocked.empty() &&
-               openBrackets.top() < unlocked.top()) {
-            openBrackets.pop();
-            unlocked.pop();
+        // Match remaining open brackets with unlocked characters.
+        int balance = 0;
+        for (int i = length - 1; i >= 0; i--) {
+            if (locked[i] == '0') {
+                balance--;
+                unlocked--;
+            } else if (s[i] == '(') {
+                balance++;
+                openBrackets--;
+            } else if (s[i] == ')') {
+                balance--;
+            }
+            if (balance > 0) {
+                return false;
+            }
+            if (unlocked == 0 and openBrackets == 0) {
+                break;
+            }
         }
 
-        if (!openBrackets.empty()) {
+        if (openBrackets > 0) {
             return false;
         }
 
