9223b0812f71e7d0408ee8047f0447a49d5cf7dd
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-01-13 20:14:03 +0530

    Time: 44 ms (45.16%), Space: 32.7 MB (21.37%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -7,12 +7,10 @@ public:
             charFrequencyMap[currentChar]++;
         }
 
-        // Step 2: Calculate the number of characters to delete
         int deleteCount = 0;
         for (auto& pair : charFrequencyMap) {
             int frequency = pair.second;
             if (frequency % 2 == 1) {
-                // If frequency is odd, delete all except one
                 deleteCount += frequency - 1;
             } else {
                 deleteCount += frequency - 2;
