759a3261013e5cd1eeda3a43154655983ce9515c
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-01-13 20:13:52 +0530

    Time: 39 ms (54.44%), Space: 32.6 MB (36.69%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -15,12 +15,10 @@ public:
                 // If frequency is odd, delete all except one
                 deleteCount += frequency - 1;
             } else {
-                // If frequency is even, delete all except two
                 deleteCount += frequency - 2;
             }
         }
 
-        // Step 3: Return the minimum length after deletions
         return s.length() - deleteCount;
     }
 };
