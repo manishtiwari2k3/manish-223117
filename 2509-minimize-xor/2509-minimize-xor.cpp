775d67a0737fddca90965cf0fca86192c58f4b15
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-01-15 21:51:28 +0530

    Time: 0 ms (100%), Space: 8.1 MB (40.19%) - LeetHub

--- 2509-minimize-xor/2509-minimize-xor.cpp
+++ 2509-minimize-xor/2509-minimize-xor.cpp
@@ -46,23 +46,19 @@ public:
 
         // While x has fewer set bits than num2
         while (setBitsCount < targetSetBitsCount) {
-            // If the current bit of num1 is set or we must set all remaining
-            // bits in result
             if (isSet(num1, currentBit) ||
                 (targetSetBitsCount - setBitsCount > currentBit)) {
                 setBit(result, currentBit);
                 setBitsCount++;
             }
-            currentBit--;  // Move to the next bit.
+            currentBit--; 
         }
 
         return result;
     }
 
 private:
-    // Helper function to check if the given bit position in x is set (1).
     bool isSet(int x, int bit) { return x & (1 << bit); }
 
-    // Helper function to set the given bit position in x to 1.
     void setBit(int &x, int bit) { x |= (1 << bit); }
 };
