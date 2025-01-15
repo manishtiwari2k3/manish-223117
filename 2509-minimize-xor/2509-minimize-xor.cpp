0b4549275c45d1d5396fad7bc44bd2fc71118dc4
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-01-15 21:51:11 +0530

    Time: 0 ms (100%), Space: 8.2 MB (10.75%) - LeetHub

--- 2509-minimize-xor/2509-minimize-xor.cpp
+++ 2509-minimize-xor/2509-minimize-xor.cpp
@@ -1,36 +1,68 @@
+// class Solution {
+// public:
+//     int minimizeXor(int num1, int num2) {
+//         int result = num1;
+
+//         int targetSetBitsCount = __builtin_popcount(num2);
+//         int setBitsCount = __builtin_popcount(result);
+
+//         int currentBit = 0;
+
+//         while (setBitsCount < targetSetBitsCount) {
+//             if (!isSet(result, currentBit)) {
+//                 setBit(result, currentBit);
+//                 setBitsCount++;
+//             }
+//             currentBit++;
+//         }
+
+//         while (setBitsCount > targetSetBitsCount) {
+//             if (isSet(result, currentBit)) {
+//                 unsetBit(result, currentBit);
+//                 setBitsCount--;
+//             }
+//             currentBit++;
+//         }
+
+//         return result;
+//     }
+
+// private:
+//     bool isSet(int x, int bit) { return x & (1 << bit); }
+
+//     void setBit(int &x, int bit) { x |= (1 << bit); }
+
+//     void unsetBit(int &x, int bit) { x &= ~(1 << bit); }
+// };
+
 class Solution {
 public:
     int minimizeXor(int num1, int num2) {
-        int result = num1;
+        int result = 0;
 
         int targetSetBitsCount = __builtin_popcount(num2);
-        int setBitsCount = __builtin_popcount(result);
+        int setBitsCount = 0;
+        int currentBit = 31;  // Start from the most significant bit.
 
-        int currentBit = 0;
-
+        // While x has fewer set bits than num2
         while (setBitsCount < targetSetBitsCount) {
-            if (!isSet(result, currentBit)) {
+            // If the current bit of num1 is set or we must set all remaining
+            // bits in result
+            if (isSet(num1, currentBit) ||
+                (targetSetBitsCount - setBitsCount > currentBit)) {
                 setBit(result, currentBit);
                 setBitsCount++;
             }
-            currentBit++;
-        }
-
-        while (setBitsCount > targetSetBitsCount) {
-            if (isSet(result, currentBit)) {
-                unsetBit(result, currentBit);
-                setBitsCount--;
-            }
-            currentBit++;
+            currentBit--;  // Move to the next bit.
         }
 
         return result;
     }
 
 private:
+    // Helper function to check if the given bit position in x is set (1).
     bool isSet(int x, int bit) { return x & (1 << bit); }
 
+    // Helper function to set the given bit position in x to 1.
     void setBit(int &x, int bit) { x |= (1 << bit); }
-
-    void unsetBit(int &x, int bit) { x &= ~(1 << bit); }
 };
