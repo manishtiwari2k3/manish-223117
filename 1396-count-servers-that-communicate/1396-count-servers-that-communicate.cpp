aec12ec1d111986ba852ebc86ead25e4f32c5d41
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-01-23 18:37:22 +0530

    Time: 2 ms (60.1%), Space: 26.6 MB (73.49%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
@@ -1,63 +1,94 @@
+// // class Solution {
+// // public:
+// //     int countServers(vector<vector<int>>& grid) {
+// //         int numRows = grid.size(), numCols = grid[0].size();
+// //         int communicableServersCount = 0;
+
+// //         for (int row = 0; row < numRows; ++row) {
+// //             for (int col = 0; col < numCols; ++col) {
+// //                 if (grid[row][col] == 1) {
+// //                     bool canCommunicate = false;
+
+// //                     for (int otherCol = 0; otherCol < numCols; ++otherCol) {
+// //                         if (otherCol != col && grid[row][otherCol] == 1) {
+// //                             canCommunicate = true;
+// //                             break;
+// //                         }
+// //                     }
+
+// //                     if (canCommunicate) {
+// //                         communicableServersCount++;
+// //                     } else {
+// //                         for (int otherRow = 0; otherRow < numRows; ++otherRow) {
+// //                             if (otherRow != row && grid[otherRow][col] == 1) {
+// //                                 canCommunicate = true;
+// //                                 break;
+// //                             }
+// //                         }
+
+// //                         if (canCommunicate) {
+// //                             communicableServersCount++;
+// //                         }
+// //                     }
+// //                 }
+// //             }
+// //         }
+
+// //         return communicableServersCount;
+// //     }
+// // };
+
+
 // class Solution {
 // public:
-//     int countServers(vector<vector<int>>& grid) {
-//         int numRows = grid.size(), numCols = grid[0].size();
+//     int countServers(const vector<vector<int>>& grid) {
+//         vector<int> rowCounts(size(grid[0]), 0), colCounts(size(grid), 0);
+
+//         for (int row = 0; row < size(grid); ++row)
+//             for (int col = 0; col < size(grid[0]); ++col)
+//                 if (grid[row][col]) {
+//                     rowCounts[col]++;
+//                     colCounts[row]++;
+//                 }
+
 //         int communicableServersCount = 0;
 
-//         for (int row = 0; row < numRows; ++row) {
-//             for (int col = 0; col < numCols; ++col) {
-//                 if (grid[row][col] == 1) {
-//                     bool canCommunicate = false;
-
-//                     for (int otherCol = 0; otherCol < numCols; ++otherCol) {
-//                         if (otherCol != col && grid[row][otherCol] == 1) {
-//                             canCommunicate = true;
-//                             break;
-//                         }
-//                     }
-
-//                     if (canCommunicate) {
-//                         communicableServersCount++;
-//                     } else {
-//                         for (int otherRow = 0; otherRow < numRows; ++otherRow) {
-//                             if (otherRow != row && grid[otherRow][col] == 1) {
-//                                 canCommunicate = true;
-//                                 break;
-//                             }
-//                         }
-
-//                         if (canCommunicate) {
-//                             communicableServersCount++;
-//                         }
-//                     }
-//                 }
-//             }
-//         }
+//         for (int row = 0; row < size(grid); ++row)
+//             for (int col = 0; col < size(grid[0]); ++col)
+//                 if (grid[row][col])
+//                     communicableServersCount +=
+//                         rowCounts[col] > 1 || colCounts[row] > 1;
 
 //         return communicableServersCount;
 //     }
 // };
 
-
 class Solution {
 public:
     int countServers(const vector<vector<int>>& grid) {
-        vector<int> rowCounts(size(grid[0]), 0), colCounts(size(grid), 0);
+        int communicableServersCount = 0;
+        vector<int> rowCounts(size(grid[0]), 0),
+            lastServerInCol(size(grid), -1);
 
-        for (int row = 0; row < size(grid); ++row)
+        // First pass to count servers in each row and column
+        for (int row = 0; row < size(grid); ++row) {
+            int serverCountInRow = 0;
             for (int col = 0; col < size(grid[0]); ++col)
                 if (grid[row][col]) {
+                    serverCountInRow++;
                     rowCounts[col]++;
-                    colCounts[row]++;
+                    lastServerInCol[row] = col;
                 }
+            // If there is more than one server in the row, increase the count
+            if (serverCountInRow != 1)
+                communicableServersCount += serverCountInRow,
+                    lastServerInCol[row] = -1;
+        }
 
-        int communicableServersCount = 0;
-
+        // Second pass to check if servers can communicate
         for (int row = 0; row < size(grid); ++row)
-            for (int col = 0; col < size(grid[0]); ++col)
-                if (grid[row][col])
-                    communicableServersCount +=
-                        rowCounts[col] > 1 || colCounts[row] > 1;
+            communicableServersCount += lastServerInCol[row] != -1 &&
+                                        rowCounts[lastServerInCol[row]] > 1;
 
         return communicableServersCount;
     }
