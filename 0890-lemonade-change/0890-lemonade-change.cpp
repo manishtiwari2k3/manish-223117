ff99560ed75ad86f4800d71507e31a5001cd616c
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-01-26 23:21:06 +0530

    Time: 0 ms (100%), Space: 87.1 MB (93.94%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0 , ten = 0;
+        for(int i=0;i<bills.size();i++){
+            if(bills[i]==5){
+            five += 1;
+        }
+        else if(bills[i]==10){
+            if(five>=1){
+                ten +=1;
+                five -= 1;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            if(five && ten){
+                // int tw += 1;
+                five -= 1;
+                ten -= 1; 
+            }
+            else if(five >= 3){
+                // tw += 1;
+                five -= 3;
+
+            }
+            else{
+                return false;
+            }
+        }
+
+        }
+        
+        return true;
+    }
+};
