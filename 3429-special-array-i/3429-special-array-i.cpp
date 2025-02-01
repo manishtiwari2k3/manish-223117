0dd71cbb86efcdd3c960cc543b88ad49ab42f398
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-02-01 23:54:29 +0530

    Time: 0 ms (100%), Space: 27.7 MB (61.71%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -18,14 +18,10 @@ public:
         for (int index = 0; index < nums.size() - 1; index++) {
             // Compare the parities of the current and next number
             if ((nums[index] & 1) ^ (nums[index + 1] & 1) == 0) {
-                // If the two adjacent numbers have the same parity, return
-                // false
                 return false;
             }
         }
 
-        // Return true if no pair of adjacent numbers with the same parity are
-        // found
         return true;
     }
 };
