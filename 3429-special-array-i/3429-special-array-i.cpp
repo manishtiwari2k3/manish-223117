dcbae3bbc7d53307f1296494cad38bd55b2c867f
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-02-01 23:54:40 +0530

    Time: 0 ms (100%), Space: 27.6 MB (86.97%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -16,7 +16,6 @@ public:
     bool isArraySpecial(vector<int>& nums) {
         // Iterate through indexes 0 to n - 1
         for (int index = 0; index < nums.size() - 1; index++) {
-            // Compare the parities of the current and next number
             if ((nums[index] & 1) ^ (nums[index + 1] & 1) == 0) {
                 return false;
             }
