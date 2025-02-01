137cdb1754a63b51c86770c437818c944598b768
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-02-01 23:53:49 +0530

    Time: 0 ms (100%), Space: 27.6 MB (61.71%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -3,7 +3,6 @@ public:
     bool isArraySpecial(vector<int>& nums) {
         for (int index = 0; index < nums.size() - 1; index++) {
             if (nums[index] % 2 == nums[index + 1] % 2) {
-                // false
                 return false;
             }
         }
