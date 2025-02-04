76bbf938e16abff595b92c5cda114ee22ba2b132
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-02-05 03:20:31 +0530

    Time: 0 ms (100%), Space: 11 MB (77.91%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.cpp
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.cpp
@@ -3,12 +3,9 @@ public:
     int maxAscendingSum(vector<int>& nums) {
         int maxSum = 0;
 
-        // Outer loop to start from each element in the array
         for (int startIdx = 0; startIdx < nums.size(); startIdx++) {
             int currentSubarraySum = nums[startIdx];
 
-            // Inner loop to check the next elements forming an ascending
-            // subarray
             for (int endIdx = startIdx + 1;
                  endIdx < nums.size() && nums[endIdx] > nums[endIdx - 1];
                  endIdx++) {
