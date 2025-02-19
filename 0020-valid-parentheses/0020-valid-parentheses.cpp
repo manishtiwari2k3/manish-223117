d96b0a69729b4abaa653eb7bec8b83a8a3eef5ff
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-02-19 10:24:04 +0530

    Time: 0 ms (100%), Space: 8.9 MB (21.29%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
+    bool isPair(char l , char i){
+        return (l=='(' && i==')') || (l=='{' && i=='}') || (l=='[' && i==']');
+    }
     bool isValid(string s) {
         stack <char> st;
         for(int i=0;i<s.length();i++){
-            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[' ){
                 st.push(s[i]);
             }
-            else if(st.empty()){
-                return false;
-            }
-            else if(!st.empty()){
-                if(s[i]==')' && st.top()!='('){
+            else{
+                if(st.empty()){
                     return false;
                 }
-                else if(s[i]=='}' && st.top()!='{'){
-                    return false;
+                if(!st.empty()){
+                    char l = st.top();
+                    if(isPair(l,s[i])){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
                 }
-                else if(s[i]==']' && st.top()!='['){
-                    return false;
-                }
-                st.pop();
             }
         }
         return st.empty();
