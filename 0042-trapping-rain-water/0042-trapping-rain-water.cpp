1e05558e929cd518eb43a21dec072aba932e05e0
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-02-24 21:04:43 +0530

    Time: 0 ms (100%), Space: 27.3 MB (32.1%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,25 +2,23 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        int lMax=0, rMax=0 , total = 0 , l = 0 , r =n-1;
-        while(l<r){
-            if(height[l]<=height[r]){
-                if(lMax>height[l]){
-                    total += lMax-height[l];
-                }
-                else{
-                    lMax = height[l];
-                }
-                l=l+1;
-            }
-            else{
-                if(rMax>height[r]){
-                    total += rMax - height[r];
-                }
-                else{
-                    rMax = height[r];
-                }
-                r = r-1;
+        vector <int> preS(n,0);
+        preS[0] = height[0];
+        for(int i=1;i<n;i++){
+            preS[i] = max(preS[i-1],height[i]);
+        }
+        vector <int> posS(n,0);
+        posS[n-1] = height[n-1];
+        for(int i=n-2;i>=0;i--){
+            posS[i] = max(posS[i+1],height[i]);
+        }
+
+        int total = 0;
+        for(int i=0;i<n;i++){
+            int lm = preS[i] ;
+            int rm = posS[i] ;
+            if(height[i]<preS[i] && height[i]<posS[i]){
+                total+= min(lm,rm)-height[i];
             }
         }
         return total;
