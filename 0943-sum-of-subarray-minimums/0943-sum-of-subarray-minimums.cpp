03b4d794417a0fc14e244c22ce044bf190ce91b1
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-02-26 13:58:45 +0530

    Time: 16 ms (61.78%), Space: 47.3 MB (44.43%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -2,50 +2,45 @@ class Solution {
 public:
     vector <int> pse(vector<int>& arr){
         int n = arr.size();
-        vector<int> ansp(n,-1);
+        vector<int>ans(n,-1);
         stack<int>st;
         for(int i=0;i<n;i++){
             while(!st.empty() && arr[st.top()]>=arr[i]){
-                    st.pop();
+                st.pop();
             }
             if(!st.empty()){
-                ansp[i] = st.top();
+                ans[i] = st.top();
             }
-            // else{
-            //     ansp[i] = st.top();
-            // }
-            st.push(i);      
+            st.push(i);
         }
-        return ansp;
+        return ans;
     }
     vector <int> nse(vector<int>& arr){
         int n = arr.size();
-        vector<int> ansn(n,n);
+        vector<int>ans(n,n);
         stack<int>st;
         for(int i=n-1;i>=0;i--){
             while(!st.empty() && arr[st.top()]>arr[i]){
-                    st.pop();
+                st.pop();
             }
             if(!st.empty()){
-                ansn[i] = st.top();
+                ans[i] = st.top();
             }
-            // else{
-            //     ansn[i] = st.top();
-            // }
-            st.push(i);      
+            st.push(i);
         }
-        return ansn;
+        return ans;
     }
     int sumSubarrayMins(vector<int>& arr) {
         int n = arr.size();
+        int total = 0;
+        const int mod = 1e9+7;
         vector<int>psei = pse(arr);
-        vector<int>nsei =nse(arr);
-        long long total = 0;
-        int mod = 1e9+7;
+        vector<int>nsei = nse(arr);
         for(int i=0;i<n;i++){
-            int left = (psei[i] == -1) ? i + 1 : i - psei[i];
-            int right = (nsei[i] == n) ? n - i : nsei[i] - i;
-            total = (total + (1LL*left*right*arr[i])%mod)%mod;
+            int left = (psei[i]==-1) ? i+1 : i-psei[i];
+            int right = (nsei[i]==n) ? n-i : nsei[i]-i;
+            total = (total+(1LL*left*right *arr[i])%mod)%mod;
+
         }
         return total;
     }
