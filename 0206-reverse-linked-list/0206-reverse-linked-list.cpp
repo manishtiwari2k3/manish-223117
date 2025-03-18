af248e9936267f0b15f86b1ee658da16cb6d882c
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-03-18 10:32:00 +0530

    Time: 0 ms (100%), Space: 13.8 MB (9.85%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,72 +11,18 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        // ListNode* temp = head;
-        // stack <int> st;
-        // while(temp){
-        //     st.push(temp->val);
-        //     temp = temp->next;
-        // }
-        // temp = head;
-        // while(temp){
-        //     temp->val = st.top();
-        //     st.pop();
-        //     temp = temp->next;
-        // }
-        // return head;
-
-        //     if(head==NULL || head->next==NULL){
-        //         return head;
-        //     }
-        //     ListNode* temp = head;
-        //     ListNode* prev = NULL;
-        //     while(temp != NULL){
-        //         ListNode* front = temp->next;
-        //         temp->next = prev;
-        //         prev = temp;
-        //         temp = front;
-        //     }
-        // return prev;
-
-        // if(head==NULL || head->next==NULL){
-        //     return head;
-        // }
-        // ListNode* temp = head;
-        // ListNode* prev = NULL;
-        // while(temp!=NULL){
-        //     ListNode* front = temp->next;
-        //     temp->next = prev;
-        //     prev = temp;
-        //     temp = front;
-        // }
-        // return prev;
-
-        // // recursive approach
-        
-
-        // // ListNode* reverseLL(ListNode* head) {
-        //     if (head == NULL || head->next == NULL) {
-        //         return head;
-        //     }
-        //     ListNode* newH = reverseList(head->next);
-
-        //     ListNode* front = head->next;
-        //     front->next = head;
-        //     head->next = NULL;
-
-        //     return newH;
-        // // }
-        
-
-        if(head==NULL||head->next==NULL){
-            return head;
+        ListNode* temp = head;
+        stack<int>st;
+        while(temp){
+            st.push(temp->val);
+            temp = temp->next;
         }
-        ListNode* newH = reverseList(head->next);
-
-        ListNode* front = head->next;
-        front->next = head;
-        head->next = NULL;
-
-        return newH;
+        temp=head;
+        while(temp){
+            temp->val = st.top();
+            st.pop();
+            temp = temp->next;
+        }
+        return head;
     }
 };
