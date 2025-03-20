40540ab30b518fd52a7f7e19c6fe719673fd2481
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-03-20 10:16:49 +0530

    Time: 0 ms (100%), Space: 14.9 MB (91.64%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -10,81 +10,42 @@
  */
 class Solution {
 public:
+    ListNode* reverseLL(ListNode* head){
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode* temp = head;
+        ListNode* back = nullptr;
+        while(temp){
+            ListNode* front = temp->next;
+            temp->next = back;
+            back = temp;
+            temp = front;
+        }
+        return back;
+    }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        // ListNode* temp = head;
-        // int cnt = 0;
-        // while(temp!=NULL){
-        //     cnt++;
-        //     temp=temp->next;
-        // }
-        // int res = cnt-n;
-        // if(cnt==n){
-        //     ListNode* newH = head->next;
-        //     delete(head);
-        //     return newH;
-        // }
-        // temp = head;
-        // while(temp){
-        //     res--;
-        //     if(res==1){
-        //         break;
-        //     }
-        //     temp = temp->next;                      wrong
-        // }
-        // ListNode* del = temp->next;
-        // temp->next = temp->next->next;
-        // delete(del);
-
-        // return head;
-
-
-
-
-
-        // ListNode* temp = head;
-        // int cnt = 0;
-        // while(temp){
-        //     cnt++;
-        //     temp = temp->next;
-        // }
-        // temp = head;
-        // if(cnt==n){
-        //     ListNode* newH = temp->next;
-        //     delete(head);
-        //     return newH;
-        // }
-        // int res = cnt-n;
-        // while(temp){
-        //     res--;
-        //     if(res==0){
-        //         break;
-        //     }
-        //     temp = temp->next;
-        // }
-        // ListNode* del = temp->next;
-        // temp->next = temp->next->next;
-        // delete(del);
-        // return head;
-
-
-        // optimal
-
-
-        ListNode * fast = head;
-        ListNode* slow = head;
-        for(int i=0;i<n;i++){
-            fast = fast->next;
+        head = reverseLL(head);
+        ListNode* temp = head;
+        ListNode* prev = nullptr;
+        int count = 0;
+        if(n==1){
+            head=head->next;
         }
-        if(fast==NULL){
-            return head->next;
+        while(temp){
+            count++;
+            if(count == n){
+                if(prev==nullptr){
+                    head = temp->next;
+                }
+                else{
+                    prev->next = temp->next;
+                }
+            }
+            prev = temp;
+            temp = temp->next;
         }
-        while(fast->next!=NULL){
-            fast=fast->next;
-            slow = slow->next;
-        }
-        ListNode* del = slow->next;
-        slow->next = slow->next->next;
-        delete(del);
-        return head;
+        
+        return reverseLL(head);;
     }
 };
