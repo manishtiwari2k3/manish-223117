66c00573d9fcce5d73c62d71509e2f84960a31f5
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-04-10 18:24:04 +0530

    Time: 0 ms (100%), Space: 15.7 MB (67.89%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int ind = -1;
+        int n = nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind = i;
+                break;
+            }
+        }
+        if(ind==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        for(int i=n-1;i>=ind;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
+        reverse(nums.begin()+ind+1,nums.end());
+        
     }
 };
-// class Solution {
-// public:
-//     void allPerm(vector<int>& nums,vector<vector<int>>ans,vector<int>ds,int freq){
-//         if(ds.size()==nums.size()){
-//             ans.push_back(ds);
-//             return;
-//         }
-//         for(int i=0;i<nums.size();i++){
-//             if(freq[i]==0){
-//                 ds.push_back(nums[i]);
-//                 freq[i] = 1;
-//                 allPerm(nums,ans,ds, freq);
-//                 freq[i] = 0;
-//                 ds.pop_back();
-//             }
-//         }
-
-//     }
-//     vector<vector<int>>nextP(vector<int>& nums){
-//         vector<vector<int>>ans;
-//         vector<int>ds;
-//         int freq[nums.size()];
-//         for(int i=0;i<nums.size();i++){
-//             freq[i] = 0;
-//         }
-//         allPerm(nums,ans,ds, freq);
-//         return ans;
-//     }
-//     void nextPermutation(vector<int>& nums) {
-//         vector<vector<int>>ls = nextP(nums);
-        
-//     }
-// };
