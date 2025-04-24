f1e9e49693cd8a7da1a622842ef29af93a218065
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-04-24 08:25:32 +0530

    Time: 0 ms (100%), Space: 18 MB (89.41%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int r, int c, int color, int newColor) {
-        if (image[r][c] == color) {
-            image[r][c] = newColor;
-            if (r >= 1) {
-                dfs(image, r - 1, c, color, newColor);
-            }
-            if (c >= 1) {
-                dfs(image, r, c - 1, color, newColor);
-            }
-            if (r + 1 < image.size()) {
-                dfs(image, r + 1, c, color, newColor);
-            }
-            if (c + 1 < image[0].size()) {
-                dfs(image, r, c + 1, color, newColor);
-            }
+    void dfs(vector<vector<int>>& image , vector<vector<int>>&ans , int delrow[] , int  delCol[] , int color , int iniColor , int row, int col){
+        ans[row][col] = color;
+        int n = image.size();
+        int m = image[0].size();
+        for(int i=0;i<4;i++){
+            int nrow = row + delrow[i];
+            int ncol = col + delCol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==iniColor && ans[nrow][ncol]!=color){
+                dfs(image , ans , delrow , delCol , color , iniColor , nrow , ncol);
+            } 
         }
-    }
 
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        int color = image[sr][sc];
-        if (color != newColor) {
-            dfs(image, sr, sc, color, newColor);
-        }
-        return image;
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        vector<vector<int>>ans = image;
+        int iniColor = image[sr][sc];
+        int delRow[] = {-1,0,1,0};
+        int delCol[] = {0,1,0,-1};
+        dfs(image , ans , delRow , delCol , color , iniColor , sr , sc);
+        return ans;
     }
 };
