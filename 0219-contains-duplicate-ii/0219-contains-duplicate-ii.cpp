37cebcb5aa36392cb30452e1a1b3e516e5b5024c
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-11-08 14:16:47 +0530

    Time: 79 ms (37.49%), Space: 98.7 MB (33.73%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -3,12 +3,12 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_map<int,int>mpp;
         for(int i=0;i<nums.size();i++){
-            if(mpp.find(nums[i])!=mpp.end()){
-                if(abs(i - mpp[nums[i]]) <= k){
+             if(mpp.find(nums[i])!=mpp.end()){
+                if(abs(i - mpp[nums[i]])<= k){
                     return true;
                 }
-            }
-            mpp[nums[i]] = i;
+             }
+             mpp[nums[i]] = i;
         }
         return false;
     }
